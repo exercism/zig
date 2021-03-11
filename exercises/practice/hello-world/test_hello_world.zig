fa00f485301e192b7bfc529c1d91b645b2f41976
massivelivefun <matthewlancefuller@gmail.com>
2021-03-11 04:38:38 -0800

    Add hello-world practice exercise (#28)
    * Add instructions.md for hello-world
    
    * Add .meta/ files for hello-world
    
    * Add solution file for hello-world
    
    * Add test file for hello-world
    
    * Add hello-world into practice exercises
    
    * Add zig symlink checking to CI test runner
    
    * Update tests.toml to new specification

---
+++ exercises/practice/hello-world/test_hello_world.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const testing = std.testing;
+
+const hello_world = @import("hello_world.zig");
+
+test "say hi" {
+    const expected = "Hello, world!";
+    const actual = comptime hello_world.hello();
+    comptime testing.expectEqualStrings(expected, actual);
+}
