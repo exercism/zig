cc54f637b3503913f97f01ffbefb2c1dc334bb97
massivelivefun <matthewlancefuller@gmail.com>
2021-03-16 14:21:04 -0700

    Add collatz-conjecture practice exercise (#32)
    * Add instructions.md for collatz-conjecture
    
    * Add .meta/ files for collatz-conjecture
    
    * Add solution file for collatz-conjecture
    
    * Add unit tests file for collatz-conjecture
    
    * Add collatz-conjecture into practice exercises
    
    * Add newline at the end of config.json
    
    * Change error set usage in unit tests file

---
+++ exercises/practice/collatz-conjecture/.meta/example.zig
@@ -0,0 +1,20 @@
+pub const ComputationError = error {
+    IllegalArgument,
+};
+
+pub fn steps(start: isize) ComputationError!isize {
+    if (start <= 0) {
+        return ComputationError.IllegalArgument;
+    }
+    var number = start;
+    var count: isize = 0;
+    while (number > 1) {
+        if (@mod(number, 2) == 0) {
+            number = @divTrunc(number, 2);
+        } else {
+            number = 3 * number + 1;
+        }
+        count += 1;
+    }
+    return count;
+}
---
+++ exercises/practice/collatz-conjecture/test_collatz_conjecture.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const testing = std.testing;
+
+const collatz_conjecture = @import("collatz_conjecture.zig");
+const ComputationError = collatz_conjecture.ComputationError;
+
+test "zero steps for one" {
+    const expected = 0;
+    const actual = comptime try collatz_conjecture.steps(1);
+    comptime testing.expectEqual(expected, actual);
+}
+
+test "divide if even" {
+    const expected = 4;
+    const actual = comptime try collatz_conjecture.steps(16);
+    comptime testing.expectEqual(expected, actual);
+}
+
+test "even and odd steps" {
+    const expected = 9;
+    const actual = comptime try collatz_conjecture.steps(12);
+    comptime testing.expectEqual(expected, actual);
+}
+
+test "large number of even and odd steps" {
+    const expected = 152;
+    const actual = comptime try collatz_conjecture.steps(1000000);
+    comptime testing.expectEqual(expected, actual);
+}
+
+test "zero is an error" {
+    const expected = ComputationError.IllegalArgument;
+    const actual = comptime collatz_conjecture.steps(0);
+    testing.expectError(expected, actual);
+}
+
+test "negative value is an error" {
+    const expected = ComputationError.IllegalArgument;
+    const actual = comptime collatz_conjecture.steps(-15);
+    testing.expectError(expected, actual);
+}
