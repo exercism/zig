834342d7b04425c6bd3c6ecad1362f612fd3e0a2
massivelivefun <matthewlancefuller@gmail.com>
2021-03-15 19:00:34 -0700

    Add solution file for grains

---
+++ exercises/practice/grains/grains.zig
@@ -0,0 +1,7 @@
+pub fn square(index: isize) ChessboardError!u64 {
+    @panic("please implement the square function");
+}
+
+pub fn total() u64 {
+    @panic("please implement the total function");
+}
