2415ac5b8aa407d50b9cadf07f8edcc8f3554e9c
massivelivefun <matthewlancefuller@gmail.com>
2021-03-16 22:19:38 -0700

    Add resistor-color-duo practice exercise (#34)
    * Add instructions.md for resistor-color-duo
    
    * Add .meta/ files for resistor-color-duo
    
    * Add solution file for resistor-color-duo
    
    * Add unit tests file for resistor-color-duo
    
    * Add resistor-color-duo into practice exercises

---
+++ exercises/practice/resistor-color-duo/.meta/example.zig
@@ -0,0 +1,24 @@
+pub const ColorBand = enum(u4) {
+    black,
+    brown,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+    grey,
+    white,
+};
+
+pub const RuntimeError = error {
+    IllegalArgument,
+};
+
+pub fn color_code(colors: []const ColorBand) RuntimeError!isize {
+    if (colors.len < 2) {
+        return RuntimeError.IllegalArgument;
+    }
+    return @as(isize, @enumToInt(colors[0])) * 10
+        + @as(isize, @enumToInt(colors[1]));
+}
---
+++ exercises/practice/resistor-color-duo/resistor_color_duo.zig
@@ -0,0 +1,3 @@
+pub fn color_code(colors: []const ColorBand) anyerror!isize {
+    @panic("please implement the color_code function");
+}
