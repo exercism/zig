0f4a2f1055a9e8838bcb8489e86c6ac377976029
massivelivefun <matthewlancefuller@gmail.com>
2021-03-18 10:22:06 -0700

    Add darts practice exercise (#35)
    * Add instructions.md for darts
    
    * Add .meta/ files for darts
    
    * Add solution file for darts
    
    * Add unit tests file for darts
    
    * Add darts into practice exercises

---
+++ exercises/practice/darts/.meta/example.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const math = std.math;
+
+pub const Coordinate = struct {
+    x_coord: f32,
+    y_coord: f32,
+
+    pub fn init(x_coord: f32, y_coord: f32) Coordinate {
+        return Coordinate {
+            .x_coord = x_coord,
+            .y_coord = y_coord,
+        };
+    }
+
+    pub fn score(self: Coordinate) isize {
+        var d = math.sqrt(self.x_coord * self.x_coord +
+            self.y_coord * self.y_coord);
+        if (d > 10) {
+            return 0;
+        } else if (d > 5) {
+            return 1;
+        } else if (d > 1) {
+            return 5;
+        }
+        return 10;
+    }
+};
---
+++ exercises/practice/darts/darts.zig
@@ -0,0 +1,10 @@
+pub const Coordinate = struct {
+    // This struct, as well as it's fields and methods, needs to be implemented.
+    
+    pub fn init(x_coord: f32, y_coord: f32) Coordinate {
+        @panic("please implement the init method");
+    }
+    pub fn score(self: Coordinate) isize {
+        @panic("please implement the score method");
+    }
+};
