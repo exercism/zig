d0357930797c68db7277effd9e50f7129e6a9008
massivelivefun <matthewlancefuller@gmail.com>
2021-03-17 23:46:52 -0700

    Add solution file for rna-transcription

---
+++ exercises/practice/rna-transcription/rna_transcription.zig
@@ -0,0 +1,8 @@
+// Import the appropriate standard library and modules
+
+pub fn toRna(
+    allocator: *mem.Allocator,
+    dna: []const u8
+) (RNAError || mem.Allocator.Error)![]const u8 {
+    @panic("please implement the toRna function");
+}
