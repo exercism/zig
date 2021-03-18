c50402beec20b4b9229c2a24bd9717410e98831e
massivelivefun <matthewlancefuller@gmail.com>
2021-03-18 11:55:31 -0700

    Add rna-transcription practice exercise (#40)
    * Add instruction.md for rna-transcription
    
    * Add .meta/ files for rna-transcription
    
    * Add solution file for rna-transcription
    
    * Add unit tests file for rna-transcription
    
    * Add rna-transcription into practice exercises

---
+++ exercises/practice/rna-transcription/.meta/example.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const mem = std.mem;
+
+pub const RNAError = error {
+    IllegalDNANucleotide,
+};
+
+pub fn toRna(
+    allocator: *mem.Allocator,
+    dna: []const u8
+) (RNAError || mem.Allocator.Error)![]const u8 {
+    var rna_slice = try allocator.alloc(u8, dna.len);
+    for (dna) |dna_nucleotide, i| {
+        switch (dna_nucleotide) {
+            'A' => rna_slice[i] = 'U',
+            'C' => rna_slice[i] = 'G',
+            'G' => rna_slice[i] = 'C',
+            'T' => rna_slice[i] = 'A',
+            else => {
+                allocator.free(rna_slice);
+                return RNAError.IllegalDNANucleotide;
+            },
+        }
+    }
+    return rna_slice;
+}
