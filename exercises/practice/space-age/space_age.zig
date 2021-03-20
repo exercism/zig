e43be56939cd3fb5667e13dfefc14987d5b0247c
massivelivefun <matthewlancefuller@gmail.com>
2021-03-19 20:22:21 -0700

    Add solution file for space-age

---
+++ exercises/practice/space-age/space_age.zig
@@ -0,0 +1,52 @@
+// An enum should be helpful for this exercise.
+
+pub const SpaceAge = struct {
+    // This struct, as well as it's fields and methods, needs to be
+    // implemented.
+
+    pub fn init(seconds: isize) SpaceAge {
+        @panic("please implement the init method");
+    }
+
+    fn getOrbitalPeriodInSecondsFromEarthYearsOf(planet: Planet) f64 {
+        @panic("please implement the
+            getOrbitalPeriodInSecondsFromEarthYearsOf method");
+    }
+
+    fn getOrbitalPeriodInEarthYearsOf(planet: Planet) f64 {
+        @panic("please implement the
+            getOrbitalPeriodInEarthYearsOf method");
+    }
+
+    pub fn onMercury(self: SpaceAge) f64 {
+        @panic("please implement the onMercury method");
+    }
+
+    pub fn onVenus(self: SpaceAge) f64 {
+        @panic("please implement the onVenus method");
+    }
+
+    pub fn onEarth(self: SpaceAge) f64 {
+        @panic("please implement the onEarth method");
+    }
+
+    pub fn onMars(self: SpaceAge) f64 {
+        @panic("please implement the onMars method");
+    }
+
+    pub fn onJupiter(self: SpaceAge) f64 {
+        @panic("please implement the onJupiter method");
+    }
+
+    pub fn onSaturn(self: SpaceAge) f64 {
+        @panic("please implement the onSaturn method");
+    }
+
+    pub fn onUranus(self: SpaceAge) f64 {
+        @panic("please implement the onUranus method");
+    }
+
+    pub fn onNeptune(self: SpaceAge) f64 {
+        @panic("please implement the onNeptune method");
+    }
+};
