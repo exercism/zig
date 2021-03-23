e2e7feeb7625c690a933ece2dc3c82d5c6a622da
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2021-03-23 16:06:28 -0700

    Add queen-attack practice exercise (#44)
    * Add instructions.md for queen-attack
    
    * Add .meta/ files for queen-attack
    
    * Add solution file for queen-attack
    
    * Add unit tests file for queen-attack
    
    * Add queen-attack into practice exercises

---
+++ exercises/practice/queen-attack/.meta/example.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const math = std.math;
+
+pub const QueenError = error{
+    InitializationFailure,
+    InvalidAttack,
+};
+
+pub const Queen = struct {
+    x: i8,
+    y: i8,
+
+    pub fn init(x: i8, y: i8) QueenError!Queen {
+        if (x < 0 or x > 7 or y < 0 or y > 7) {
+            return QueenError.InitializationFailure;
+        }
+        return Queen {
+            .x = x,
+            .y = y,
+        };
+    }
+
+    pub fn canAttack(self: Queen, other: Queen) QueenError!bool {
+        if (self.x == other.x and self.y == other.y) {
+            return QueenError.InvalidAttack;
+        }
+        return (self.x == other.x) or (self.y == other.y) or
+            (math.absInt(self.x - other.x) catch unreachable ==
+            math.absInt(self.y - other.y) catch unreachable);
+    }
+};
