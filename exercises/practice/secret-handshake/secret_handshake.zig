2d9ecdb8ec1c8a447078d7628cfd9efc1090f5f9
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2021-03-24 01:11:39 -0700

    Add solution file for secret-handshake

---
+++ exercises/practice/secret-handshake/secret_handshake.zig
@@ -0,0 +1,6 @@
+pub fn calculateHandshake(
+    allocator: *mem.Allocator,
+    number: isize
+) mem.Allocator.Error![]const Signal {
+    @panic("please implement the calculateHandshake function");
+}
