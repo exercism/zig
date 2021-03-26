b3513fe3f469feb3dc1aab2eb3b1a9ae13f520f1
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2021-03-26 03:00:55 -0700

    Add binary-search practice exercise (#45)
    * Add instructions.md for binary-search
    
    * Add .meta/ files for binary-search
    
    * Add solution file for binary-search
    
    * Add unit tests file for binary-search
    
    * Add binary-search into practice exercises
    
    * Add type-coercion to exercise's pracs and prereqs
    
    * Flatten binary-search authors

---
+++ exercises/practice/binary-search/binary_search.zig
@@ -0,0 +1,8 @@
+// Take a look at the tests, you might have to change the function arguments
+
+pub fn binarySearch(
+    target: usize,
+    buffer: ?[]const usize
+) SearchError!usize {
+    @panic("please implement the binarySearch function");
+}
