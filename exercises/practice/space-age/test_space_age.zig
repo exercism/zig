63033e4851de31099d1079ae08db4824bb200474
Josh Goebel <me@joshgoebel.com>
2021-12-16 17:03:54 -0500

    use newer API expectApproxEqRel

--- exercises/practice/space-age/test_space_age.zig
+++ exercises/practice/space-age/test_space_age.zig
@@ -7,54 +7,54 @@ test "age on earth" {
     const sp = comptime space_age.SpaceAge.init(1000000000);
     const expected = 31.69;
     const actual = comptime sp.onEarth();
-    comptime testing.expectWithinMargin(expected, actual, 1.0);
+    comptime try testing.expectApproxEqRel(expected, actual, 1.0);
 }
 
 test "age on mercury" {
     const sp = comptime space_age.SpaceAge.init(2134835688);
     const expected = 280.88;
     const actual = comptime sp.onMercury();
-    comptime testing.expectWithinMargin(expected, actual, 1.0);
+    comptime try testing.expectApproxEqRel(expected, actual, 1.0);
 }
 
 test "age on venus" {
     const sp = comptime space_age.SpaceAge.init(189839836);
     const expected = 9.78;
     const actual = comptime sp.onVenus();
-    comptime testing.expectWithinMargin(expected, actual, 1.0);
+    comptime try testing.expectApproxEqRel(expected, actual, 1.0);
 }
 
 test "age on mars" {
     const sp = comptime space_age.SpaceAge.init(2129871239);
     const expected = 35.88;
     const actual = comptime sp.onMars();
-    comptime testing.expectWithinMargin(expected, actual, 1.0);
+    comptime try testing.expectApproxEqRel(expected, actual, 1.0);
 }
 
 test "age on jupiter" {
     const sp = comptime space_age.SpaceAge.init(901876382);
     const expected = 2.41;
     const actual = comptime sp.onJupiter();
-    comptime testing.expectWithinMargin(expected, actual, 1.0);
+    comptime try testing.expectApproxEqRel(expected, actual, 1.0);
 }
 
 test "age on saturn" {
     const sp = comptime space_age.SpaceAge.init(2000000000);
     const expected = 2.15;
     const actual = comptime sp.onSaturn();
-    comptime testing.expectWithinMargin(expected, actual, 1.0);
+    comptime try testing.expectApproxEqRel(expected, actual, 1.0);
 }
 
 test "age on uranus" {
     const sp = comptime space_age.SpaceAge.init(1210123456);
     const expected = 0.46;
     const actual = comptime sp.onUranus();
-    comptime testing.expectWithinMargin(expected, actual, 1.0);
+    comptime try testing.expectApproxEqRel(expected, actual, 1.0);
 }
 
 test "age on neptune" {
     const sp = comptime space_age.SpaceAge.init(1821023456);
     const expected = 0.35;
     const actual = comptime sp.onNeptune();
-    comptime testing.expectWithinMargin(expected, actual, 1.0);
+    comptime try testing.expectApproxEqRel(expected, actual, 1.0);
 }
