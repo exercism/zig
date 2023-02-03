fe63abe0865d8d75f45558a875491f949e21baec
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-03 13:50:01 +0100

    exercises(hello-world): test: handle incorrect solution better
    Before this commit, when testing an incorrect solution:
    
        $ zig test test_hello_world.zig
        /foo/zig/lib/std/Thread.zig:741:30: error: unable to evaluate comptime expression
                return tls_thread_id orelse {
                       ~~~~~~~~~~~~~~^~~~~~
        /foo/zig/lib/std/Thread.zig:275:29: note: called from here
            return Impl.getCurrentId();
                   ~~~~~~~~~~~~~~~~~^~
        /foo/zig/lib/std/Thread/Mutex.zig:82:47: note: called from here
                const current_id = Thread.getCurrentId();
                                   ~~~~~~~~~~~~~~~~~~~^~
        /foo/zig/lib/std/Thread/Mutex.zig:46:19: note: called from here
            self.impl.lock();
            ~~~~~~~~~~~~~~^~
        /foo/zig/lib/std/debug.zig:90:22: note: called from here
            stderr_mutex.lock();
            ~~~~~~~~~~~~~~~~~^~
        /foo/zig/lib/std/testing.zig:611:14: note: called from here
                print("\n====== expected this output: =========\n", .{});
                ~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        test_hello_world.zig:9:44: note: called from here
            comptime try testing.expectEqualStrings(expected, actual);
                         ~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~
    
    With this commit, we get a test failure message:
    
        $ zig test test_hello_world.zig
        Test [1/1] test.say hi...
        ====== expected this output: =========
        Hello, world!␃
    
        ======== instead found this: =========
        Goodbye, Mars!␃
    
        ======================================
        First difference occurs on line 1:
        expected:
        Hello, world!
        ^ ('\x48')
        found:
        Goodbye, Mars!
        ^ ('\x47')
        Test [1/1] test.say hi... FAIL (TestExpectedEqual)

--- exercises/practice/hello-world/test_hello_world.zig
+++ exercises/practice/hello-world/test_hello_world.zig
@@ -6,5 +6,5 @@ const hello_world = @import("hello_world.zig");
 test "say hi" {
     const expected = "Hello, world!";
     const actual = comptime hello_world.hello();
-    comptime try testing.expectEqualStrings(expected, actual);
+    try testing.expectEqualStrings(expected, actual);
 }
