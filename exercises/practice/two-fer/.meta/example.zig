468b90571ecdf72d28522dfd7585d0214e4a8974
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-03 12:59:03 +0100

    install-zig, exercises: bump Zig from 0.9.1 to 0.10.1 (#108)
    * install-zig: bump Zig from 0.9.1 to 0.10.1
    
    See the release notes [1][2].
    
    [1] https://ziglang.org/download/0.10.0/release-notes.html
    [2] https://ziglang.org/download/0.10.1/release-notes.html
    
    Refs: #99
    
    * exercises(leap): example: fix for Zig 0.10.1
    
    Simplify the example solution, for which Zig 0.10.1 produced an error:
    
        $ zig test test_leap.zig
        leap.zig:8:21: error: no field named 'has_factor' in struct 'leap.leap__struct_1016'
            }{ .y = year }).has_factor;
                        ^~~~~~~~~~
        leap.zig:2:25: note: struct declared here
            const has_factor = (struct {
                            ^~~~~~
        test_leap.zig:7:43: note: called from here
            comptime try testing.expect(!leap.leap(2015));
                                         ~~~~~~~~~^~~~~~
    
    * exercises(two-fer): example: fix for Zig 0.10.1
    
    Tweak the example solution, for which Zig 0.10.1 produced an error:
    
        $ zig test -freference-trace test_two_fer.zig
        /foo/zig-linux-x86_64-0.10.1/lib/std/fmt.zig:498:17: error: cannot format optional without a specifier (i.e. {?} or {any})
                        @compileError("cannot format optional without a specifier (i.e. {?} or {any})");
                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        referenced by:
            format__anon_3843: /foo/zig-linux-x86_64-0.10.1/lib/std/fmt.zig:183:13
            bufPrint__anon_1501: /foo/zig-linux-x86_64-0.10.1/lib/std/fmt.zig:1922:9
            two_fer: two_fer.zig:6:24
            test.another name given: test_two_fer.zig:25:31

--- exercises/practice/two-fer/.meta/example.zig
+++ exercises/practice/two-fer/.meta/example.zig
@@ -3,6 +3,6 @@ const fmt = std.fmt;
 
 pub fn two_fer(buffer: []u8, name: ?[]const u8) anyerror![]u8 {
     var word = if (name == null) "me" else name;
-    var slice = try fmt.bufPrint(buffer, "One for {s}, one for you.", .{word});
+    var slice = try fmt.bufPrint(buffer, "One for {?s}, one for you.", .{word});
     return slice;
 }
