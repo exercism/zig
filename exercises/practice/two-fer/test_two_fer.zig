ef4b64f798ef08398ad6f74484a9767c7c83317c
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-07 10:10:02 +0100

    exercises: use snake_case for variable names
    From the Zig Style Guide [1]:
    
        Roughly speaking: `camelCaseFunctionName`, `TitleCaseTypeName`,
        `snake_case_variable_name`. More precisely:
    
        - If `x` is a `type` then `x` should be `TitleCase`, unless it is a
          `struct` with 0 fields and is never meant to be instantiated, in
          which case it is considered to be a "namespace" and uses
          `snake_case`.
    
        - If `x` is callable, and `x`'s return type is `type`, then `x`
          should be `TitleCase`.
    
        - If `x` is otherwise callable, then `x` should be `camelCase`.
    
        - Otherwise, `x` should be `snake_case`.
    
    [1] https://ziglang.org/documentation/0.10.1/#Style-Guide

--- exercises/practice/two-fer/test_two_fer.zig
+++ exercises/practice/two-fer/test_two_fer.zig
@@ -3,24 +3,24 @@ const testing = std.testing;
 
 const two_fer = @import("two_fer.zig");
 
-const BUFFER_SIZE = 100;
+const buffer_size = 100;
 
 test "no name given" {
-    var response: [BUFFER_SIZE]u8 = undefined;
+    var response: [buffer_size]u8 = undefined;
     const expected = "One for me, one for you.";
     const actual = try two_fer.twoFer(&response, null);
     try testing.expectEqualStrings(expected, actual);
 }
 
 test "a name given" {
-    var response: [BUFFER_SIZE]u8 = undefined;
+    var response: [buffer_size]u8 = undefined;
     const expected = "One for Alice, one for you.";
     const actual = try two_fer.twoFer(&response, "Alice");
     try testing.expectEqualStrings(expected, actual);
 }
 
 test "another name given" {
-    var response: [BUFFER_SIZE]u8 = undefined;
+    var response: [buffer_size]u8 = undefined;
     const expected = "One for Bob, one for you.";
     const actual = try two_fer.twoFer(&response, "Bob");
     try testing.expectEqualStrings(expected, actual);
