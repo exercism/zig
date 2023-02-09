cb2044b0ef9476476db740361b6434164c76699a
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-09 09:46:48 +0100

    exercises(two-fer): fix twoFer returned values (#157)
    The implementation of this exercise didn't match the instructions, which
    specify:
    
        Given a name, return a string with the message:
    
        ```text
        One for name, one for me.
        ```
    
        Where "name" is the given name.
    
        However, if the name is missing, return the string:
    
        ```text
        One for you, one for me.
        ```

--- exercises/practice/two-fer/test_two_fer.zig
+++ exercises/practice/two-fer/test_two_fer.zig
@@ -7,21 +7,21 @@ const buffer_size = 100;
 
 test "no name given" {
     var response: [buffer_size]u8 = undefined;
-    const expected = "One for me, one for you.";
+    const expected = "One for you, one for me.";
     const actual = try two_fer.twoFer(&response, null);
     try testing.expectEqualStrings(expected, actual);
 }
 
 test "a name given" {
     var response: [buffer_size]u8 = undefined;
-    const expected = "One for Alice, one for you.";
+    const expected = "One for Alice, one for me.";
     const actual = try two_fer.twoFer(&response, "Alice");
     try testing.expectEqualStrings(expected, actual);
 }
 
 test "another name given" {
     var response: [buffer_size]u8 = undefined;
-    const expected = "One for Bob, one for you.";
+    const expected = "One for Bob, one for me.";
     const actual = try two_fer.twoFer(&response, "Bob");
     try testing.expectEqualStrings(expected, actual);
 }
