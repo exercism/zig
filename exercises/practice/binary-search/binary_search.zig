9be57e9da8bd931e4d20ed4921b6facd481fc3e1
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-15 18:55:02 +0100

    exercises: run zig fmt on stubs

--- exercises/practice/binary-search/binary_search.zig
+++ exercises/practice/binary-search/binary_search.zig
@@ -1,8 +1,5 @@
 // Take a look at the tests, you might have to change the function arguments
 
-pub fn binarySearch(
-    target: usize,
-    buffer: ?[]const usize
-) SearchError!usize {
+pub fn binarySearch(target: usize, buffer: ?[]const usize) SearchError!usize {
     @panic("please implement the binarySearch function");
 }
