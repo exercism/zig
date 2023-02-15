ff5839de7079ddfc0d3e8481df07d9167ee6e677
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-15 18:00:01 +0100

    exercises(leap): rename function to isLeapYear
    With this commit, the functions that return a bool are more consistently
    named:
    
        $ git grep --break --heading 'bool'
        exercises/practice/isogram/.meta/example.zig
        1:pub fn isIsogram(str: []const u8) bool {
    
        exercises/practice/isogram/isogram.zig
        1:pub fn isIsogram(str: []const u8) bool {
    
        exercises/practice/leap/.meta/example.zig
        1:pub fn isLeapYear(year: u32) bool {
    
        exercises/practice/leap/leap.zig
        1:pub fn isLeapYear(year: u32) bool {
    
        exercises/practice/pangram/.meta/example.zig
        4:pub fn isPangram(str: []const u8) bool {
    
        exercises/practice/pangram/pangram.zig
        1:pub fn isPangram(str: []const u8) bool {
    
        exercises/practice/queen:attack/.meta/example.zig
        23:    pub fn canAttack(self: Queen, other: Queen) QueenError!bool {
    
        exercises/practice/secret:handshake/.meta/example.zig
        42:inline fn isFlipped(number: isize, bit_mask: isize) bool {
    
        exercises/practice/triangle/.meta/example.zig
        49:    pub fn isEquilateral(self: Triangle) bool {
        55:    pub fn isIsosceles(self: Triangle) bool {
        61:    pub fn isScalene(self: Triangle) bool {
    
        exercises/practice/triangle/triangle.zig
        28:    pub fn isEquilateral(self: Triangle) bool {
        32:    pub fn isIsosceles(self: Triangle) bool {
        36:    pub fn isScalene(self: Triangle) bool {

--- exercises/practice/leap/test_leap.zig
+++ exercises/practice/leap/test_leap.zig
@@ -4,37 +4,37 @@ const testing = std.testing;
 const leap = @import("leap.zig");
 
 test "year not divisible by 4 in common year" {
-    try testing.expect(!leap.leap(2015));
+    try testing.expect(!leap.isLeapYear(2015));
 }
 
 test "year divisible by 2, not divisible by 4 in common year" {
-    try testing.expect(!leap.leap(1970));
+    try testing.expect(!leap.isLeapYear(1970));
 }
 
 test "year divisible by 4, not divisible by 100 in leap year" {
-    try testing.expect(leap.leap(1996));
+    try testing.expect(leap.isLeapYear(1996));
 }
 
 test "year divisible by 4 and 5 is still a leap year" {
-    try testing.expect(leap.leap(1960));
+    try testing.expect(leap.isLeapYear(1960));
 }
 
 test "year divisible by 100, not divisible by 400 in common year" {
-    try testing.expect(!leap.leap(2100));
+    try testing.expect(!leap.isLeapYear(2100));
 }
 
 test "year divisible by 100 but not by 3 is still not a leap year" {
-    try testing.expect(!leap.leap(1900));
+    try testing.expect(!leap.isLeapYear(1900));
 }
 
 test "year divisible by 400 is leap year" {
-    try testing.expect(leap.leap(2000));
+    try testing.expect(leap.isLeapYear(2000));
 }
 
 test "year divisible by 400 but not by 125 is still a leap year" {
-    try testing.expect(leap.leap(2400));
+    try testing.expect(leap.isLeapYear(2400));
 }
 
 test "year divisible by 200, not divisible by 400 in common year" {
-    try testing.expect(!leap.leap(1800));
+    try testing.expect(!leap.isLeapYear(1800));
 }
