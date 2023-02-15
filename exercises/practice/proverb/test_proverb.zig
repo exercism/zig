ad367e74db7f88b28b0911dc4792735224d1f788
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-15 18:55:03 +0100

    exercises: run zig fmt on tests

--- exercises/practice/proverb/test_proverb.zig
+++ exercises/practice/proverb/test_proverb.zig
@@ -4,7 +4,7 @@ const testing = std.testing;
 const proverb = @import("proverb.zig");
 
 test "zero pieces" {
-    const array_long = [_][]const u8 {};
+    const array_long = [_][]const u8{};
     const input_slice = &array_long;
 
     const expected = input_slice;
@@ -23,12 +23,12 @@ test "zero pieces" {
 test "one piece" {
     const first_input = "nail".*;
     const first_slice = &first_input;
-    const input_array = [_][]const u8 { first_slice };
+    const input_array = [_][]const u8{first_slice};
     const input_slice = &input_array;
 
     const first_expected = "And all for the want of a nail.\n".*;
     const first_expected_slice = &first_expected;
-    const expected_array = [_][]const u8 { first_expected_slice };
+    const expected_array = [_][]const u8{first_expected_slice};
     const expected = &expected_array;
 
     const actual = try proverb.recite(testing.allocator, input_slice);
@@ -48,15 +48,14 @@ test "two pieces" {
     const first_slice = &first_input;
     const second_input = "shoe".*;
     const second_slice = &second_input;
-    const input_array = [_][]const u8 { first_slice, second_slice };
+    const input_array = [_][]const u8{ first_slice, second_slice };
     const input_slice = &input_array;
 
     const first_expected = "For want of a nail the shoe was lost.\n".*;
-    const first_expected_slice = &first_expected ;
+    const first_expected_slice = &first_expected;
     const second_expected = "And all for the want of a nail.\n".*;
     const second_expected_slice = &second_expected;
-    const expected_array = [_][]const u8 { first_expected_slice,
-        second_expected_slice };
+    const expected_array = [_][]const u8{ first_expected_slice, second_expected_slice };
     const expected = &expected_array;
 
     const actual = try proverb.recite(testing.allocator, input_slice);
@@ -78,8 +77,7 @@ test "three pieces" {
     const second_slice = &second_input;
     const third_input = "horse".*;
     const third_slice = &third_input;
-    const input_array = [_][]const u8 { first_slice, second_slice,
-        third_slice };
+    const input_array = [_][]const u8{ first_slice, second_slice, third_slice };
     const input_slice = &input_array;
 
     const first_expected = "For want of a nail the shoe was lost.\n".*;
@@ -88,8 +86,7 @@ test "three pieces" {
     const second_expected_slice = &second_expected;
     const third_expected = "And all for the want of a nail.\n".*;
     const third_expected_slice = &third_expected;
-    const expected_array = [_][]const u8 { first_expected_slice,
-        second_expected_slice, third_expected_slice };
+    const expected_array = [_][]const u8{ first_expected_slice, second_expected_slice, third_expected_slice };
     const expected = &expected_array;
 
     const actual = try proverb.recite(testing.allocator, input_slice);
@@ -119,8 +116,7 @@ test "full proverb" {
     const sixth_slice = &sixth_input;
     const seventh_input = "kingdom".*;
     const seventh_slice = &seventh_input;
-    const input_array = [_][]const u8 { first_slice, second_slice,
-        third_slice, fourth_slice, fifth_slice, sixth_slice, seventh_slice };
+    const input_array = [_][]const u8{ first_slice, second_slice, third_slice, fourth_slice, fifth_slice, sixth_slice, seventh_slice };
     const input_slice = &input_array;
 
     const first_expected = "For want of a nail the shoe was lost.\n".*;
@@ -137,9 +133,7 @@ test "full proverb" {
     const sixth_expected_slice = &sixth_expected;
     const seventh_expected = "And all for the want of a nail.\n".*;
     const seventh_expected_slice = &seventh_expected;
-    const expected_array = [_][]const u8 { first_expected_slice,
-        second_expected_slice, third_expected_slice, fourth_expected_slice,
-        fifth_expected_slice, sixth_expected_slice, seventh_expected_slice };
+    const expected_array = [_][]const u8{ first_expected_slice, second_expected_slice, third_expected_slice, fourth_expected_slice, fifth_expected_slice, sixth_expected_slice, seventh_expected_slice };
     const expected = &expected_array;
 
     const actual = try proverb.recite(testing.allocator, input_slice);
@@ -163,8 +157,7 @@ test "four pieces modernized" {
     const third_slice = &third_input;
     const fourth_input = "battle".*;
     const fourth_slice = &fourth_input;
-    const input_array = [_][]const u8 { first_slice, second_slice,
-        third_slice, fourth_slice };
+    const input_array = [_][]const u8{ first_slice, second_slice, third_slice, fourth_slice };
     const input_slice = &input_array;
 
     const first_expected = "For want of a pin the gun was lost.\n".*;
@@ -175,8 +168,7 @@ test "four pieces modernized" {
     const third_expected_slice = &third_expected;
     const fourth_expected = "And all for the want of a pin.\n".*;
     const fourth_expected_slice = &fourth_expected;
-    const expected_array = [_][]const u8 { first_expected_slice,
-        second_expected_slice, third_expected_slice, fourth_expected_slice };
+    const expected_array = [_][]const u8{ first_expected_slice, second_expected_slice, third_expected_slice, fourth_expected_slice };
     const expected = &expected_array;
 
     const actual = try proverb.recite(testing.allocator, input_slice);
