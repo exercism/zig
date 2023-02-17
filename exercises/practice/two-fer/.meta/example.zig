05096e9d76c55f9bb18910c8c6603b7015d4299f
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-17 16:00:01 +0100

    exercises(two-fer): improve example

--- exercises/practice/two-fer/.meta/example.zig
+++ exercises/practice/two-fer/.meta/example.zig
@@ -1,8 +1,5 @@
-const std = @import("std");
-const fmt = std.fmt;
+const fmt = @import("std").fmt;
 
-pub fn twoFer(buffer: []u8, name: ?[]const u8) anyerror![]u8 {
-    const word = if (name == null) "you" else name;
-    const slice = try fmt.bufPrint(buffer, "One for {?s}, one for me.", .{word});
-    return slice;
+pub fn twoFer(buffer: []u8, name: ?[]const u8) fmt.BufPrintError![]u8 {
+    return fmt.bufPrint(buffer, "One for {s}, one for me.", .{name orelse "you"});
 }
