8fe2dda7651ecf56a1d992fea57baf1f45b15e0e
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-17 23:47:34 +0100

    exercises(space-age): print values on test failure (#172)
    Before this commit, a failing test would not print the numbers involved:
    
        $ zig test test_space_age.zig
        Test [2/8] test.age on mercury... FAIL (TestUnexpectedResult)
        [...]/zig/lib/std/testing.zig:509:14: 0x20aaaa in expect (test)
            if (!ok) return error.TestUnexpectedResult;
                     ^
        [...]/exercism-tracks/zig/exercises/practice/space-age/test_space_age.zig:19:5: 0x20bbbb in test.age on mercury (test)
            try expect(approxEqAbs(f64, expected, actual, tolerance));
            ^
        7 passed; 0 skipped; 1 failed.
    
    With this commit, the first line of the test failure message contains
    the returned value, expected value, and tolerance:
    
        Test [2/8] test.age on mercury... actual 2.8e+02, not within absolute tolerance 2.0e-02 of expected 2.8088e+02
        Test [2/8] test.age on mercury... FAIL (TestExpectedApproxEqAbs)
        [..]/zig/lib/std/testing.zig:223:13: 0x22aaaa in expectApproxEqAbs__anon_1080 (test)
                    return error.TestExpectedApproxEqAbs;
                    ^
        [...]/exercism-tracks/zig/exercises/practice/space-age/test_space_age.zig:18:5: 0x22bbbb in test.age on mercury (test)
            try expectApproxEqAbs(expected, actual, tolerance);
            ^
        7 passed; 0 skipped; 1 failed.
    
    From the doc comment of `std.testing.expectApproxEqAbs` [1]:
    
        This function is intended to be used only in tests. When the actual
        value is not approximately equal to the expected value, prints
        diagnostics to stderr to show exactly how they are not equal, then
        returns a test failure error.
    
    [1] https://github.com/ziglang/zig/blob/7199d7c77715/lib/std/testing.zig#L212-L230
    
    Closes: #170

--- exercises/practice/space-age/test_space_age.zig
+++ exercises/practice/space-age/test_space_age.zig
@@ -1,62 +1,61 @@
 const std = @import("std");
-const expect = std.testing.expect;
-const approxEqAbs = std.math.approxEqAbs;
+const expectApproxEqAbs = std.testing.expectApproxEqAbs;
 const tolerance = 0.02;
 
 const space_age = @import("space_age.zig");
 
 test "age on earth" {
     const sp = comptime space_age.SpaceAge.init(1_000_000_000);
-    const expected = 31.69;
+    const expected: f64 = 31.69;
     const actual = comptime sp.onEarth();
-    try expect(approxEqAbs(f64, expected, actual, tolerance));
+    try expectApproxEqAbs(expected, actual, tolerance);
 }
 
 test "age on mercury" {
     const sp = comptime space_age.SpaceAge.init(2_134_835_688);
-    const expected = 280.88;
+    const expected: f64 = 280.88;
     const actual = comptime sp.onMercury();
-    try expect(approxEqAbs(f64, expected, actual, tolerance));
+    try expectApproxEqAbs(expected, actual, tolerance);
 }
 
 test "age on venus" {
     const sp = comptime space_age.SpaceAge.init(189_839_836);
-    const expected = 9.78;
+    const expected: f64 = 9.78;
     const actual = comptime sp.onVenus();
-    try expect(approxEqAbs(f64, expected, actual, tolerance));
+    try expectApproxEqAbs(expected, actual, tolerance);
 }
 
 test "age on mars" {
     const sp = comptime space_age.SpaceAge.init(2_129_871_239);
-    const expected = 35.88;
+    const expected: f64 = 35.88;
     const actual = comptime sp.onMars();
-    try expect(approxEqAbs(f64, expected, actual, tolerance));
+    try expectApproxEqAbs(expected, actual, tolerance);
 }
 
 test "age on jupiter" {
     const sp = comptime space_age.SpaceAge.init(901_876_382);
-    const expected = 2.41;
+    const expected: f64 = 2.41;
     const actual = comptime sp.onJupiter();
-    try expect(approxEqAbs(f64, expected, actual, tolerance));
+    try expectApproxEqAbs(expected, actual, tolerance);
 }
 
 test "age on saturn" {
     const sp = comptime space_age.SpaceAge.init(2_000_000_000);
-    const expected = 2.15;
+    const expected: f64 = 2.15;
     const actual = comptime sp.onSaturn();
-    try expect(approxEqAbs(f64, expected, actual, tolerance));
+    try expectApproxEqAbs(expected, actual, tolerance);
 }
 
 test "age on uranus" {
     const sp = comptime space_age.SpaceAge.init(1_210_123_456);
-    const expected = 0.46;
+    const expected: f64 = 0.46;
     const actual = comptime sp.onUranus();
-    try expect(approxEqAbs(f64, expected, actual, tolerance));
+    try expectApproxEqAbs(expected, actual, tolerance);
 }
 
 test "age on neptune" {
     const sp = comptime space_age.SpaceAge.init(1_821_023_456);
-    const expected = 0.35;
+    const expected: f64 = 0.35;
     const actual = comptime sp.onNeptune();
-    try expect(approxEqAbs(f64, expected, actual, tolerance));
+    try expectApproxEqAbs(expected, actual, tolerance);
 }
