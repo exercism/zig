3fe6ac78b1c441ca79d176c1b55de67f30edc036
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-17 20:00:02 +0100

    exercises(armstrong-numbers): countDigits: inline

--- exercises/practice/armstrong-numbers/.meta/example.zig
+++ exercises/practice/armstrong-numbers/.meta/example.zig
@@ -1,14 +1,9 @@
 const std = @import("std");
 const math = std.math;
 
-/// Returns the number of digits in `n`.
-fn countDigits(n: u64) u64 {
-    if (n == 0) return 1;
-    return 1 + @floatToInt(u64, @trunc(@log10(@intToFloat(f64, n))));
-}
-
 pub fn isArmstrongNumber(num: u64) bool {
-    const num_digits = countDigits(num);
+    if (num == 0) return true;
+    const num_digits = 1 + @floatToInt(u64, @trunc(@log10(@intToFloat(f64, num))));
     var n = num;
     var sum: u64 = 0;
     while (n > 0) {
