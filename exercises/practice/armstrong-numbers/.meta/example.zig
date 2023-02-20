af30ed70141b80a48afcc702a88b615bb2f46517
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-20 20:00:01 +0100

    exercises(armstrong-numbers): use math.log10, and add comment

--- exercises/practice/armstrong-numbers/.meta/example.zig
+++ exercises/practice/armstrong-numbers/.meta/example.zig
@@ -3,7 +3,9 @@ const math = std.math;
 
 pub fn isArmstrongNumber(num: u128) bool {
     if (num == 0) return true;
-    const num_digits = 1 + @floatToInt(u128, @trunc(@log10(@intToFloat(f64, num))));
+    // Currently, the below can produce an incorrect count for numbers near a large power of ten.
+    // For example, it produces 16 for the 15-digit 999_999_999_999_998.
+    const num_digits = 1 + math.log10(num);
     var n = num;
     var sum: u128 = 0;
     while (n > 0) {
