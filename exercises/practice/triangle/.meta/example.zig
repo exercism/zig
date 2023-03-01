20a9f6addcc0cae0352fa6eb0a8aef98fb39b003
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-01 14:20:02 +0100

    exercises(triangle): example: prefer single line

--- exercises/practice/triangle/.meta/example.zig
+++ exercises/practice/triangle/.meta/example.zig
@@ -11,46 +11,30 @@ pub const Triangle = struct {
     pub fn init(a: f64, b: f64, c: f64) TriangleError!Triangle {
         try verifyIfDegenerateAttributesExist(a, b, c);
         try verifyIfTriangleInequalityHolds(a, b, c);
-        return Triangle{
-            .a = a,
-            .b = b,
-            .c = c,
-        };
+        return Triangle{ .a = a, .b = b, .c = c };
     }
 
     fn verifyIfDegenerateAttributesExist(a: f64, b: f64, c: f64) TriangleError!void {
-        if ((a + b == c) or
-            (a + c == b) or
-            (b + c == a))
-        {
+        if ((a + b == c) or (a + c == b) or (b + c == a)) {
             return TriangleError.Degenerate;
         }
     }
 
     fn verifyIfTriangleInequalityHolds(a: f64, b: f64, c: f64) TriangleError!void {
-        if ((a + b < c) or
-            (a + c < b) or
-            (b + c < a))
-        {
+        if ((a + b < c) or (a + c < b) or (b + c < a)) {
             return TriangleError.InvalidInequality;
         }
     }
 
     pub fn isEquilateral(self: Triangle) bool {
-        return (self.a == self.b) and
-            (self.b == self.c) and
-            (self.a == self.c);
+        return (self.a == self.b) and (self.b == self.c) and (self.a == self.c);
     }
 
     pub fn isIsosceles(self: Triangle) bool {
-        return (self.a == self.b) or
-            (self.b == self.c) or
-            (self.a == self.c);
+        return (self.a == self.b) or (self.b == self.c) or (self.a == self.c);
     }
 
     pub fn isScalene(self: Triangle) bool {
-        return (self.a != self.b) and
-            (self.b != self.c) and
-            (self.a != self.c);
+        return (self.a != self.b) and (self.b != self.c) and (self.a != self.c);
     }
 };
