d7cb6ca60bc45f6d8a35860a16cf3eb8e168e495
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-03 16:46:12 +0100

    exercises: tests: remove comptime (#211)
    Rationale:
    
    - There is no more `comptime` in the tests that can potentially confuse
      a user. It was required in some of these exercises, e.g. when we wrote
      something like `expected = 42`.
    
    - Test failure messages at comptime can sometimes be more confusing [1].
    
    - The main downside is that, for some exercises, we no longer support
      the user returning a signed integer instead of an unsigned integer.
      But that's OK.
    
    With this commit, the only remaining occurrence of `comptime` in this
    repo is in the `binary-search` example solution:
    
        $ git grep --break --heading comptime
        exercises/practice/binary:search/.meta/example.zig
        7:pub fn binarySearch(comptime T: type, target: T, buffer: ?[]const T) SearchError!usize {
    
    [1] 7dc446e8b0a6 "exercises(hello-world): test: handle incorrect solution
        better", 2023-02-03

--- exercises/practice/darts/test_darts.zig
+++ exercises/practice/darts/test_darts.zig
@@ -4,92 +4,92 @@ const testing = std.testing;
 const darts = @import("darts.zig");
 
 test "missed target" {
-    const coordinate = comptime darts.Coordinate.init(-9.0, 9.0);
-    const expected = 0;
-    const actual = comptime coordinate.score();
+    const expected: usize = 0;
+    const coordinate = darts.Coordinate.init(-9.0, 9.0);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "on the outer circle" {
-    const coordinate = comptime darts.Coordinate.init(0.0, 10.0);
-    const expected = 1;
-    const actual = comptime coordinate.score();
+    const expected: usize = 1;
+    const coordinate = darts.Coordinate.init(0.0, 10.0);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "on the middle circle" {
-    const coordinate = comptime darts.Coordinate.init(-5.0, 0.0);
-    const expected = 5;
-    const actual = comptime coordinate.score();
+    const expected: usize = 5;
+    const coordinate = darts.Coordinate.init(-5.0, 0.0);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "on the inner circle" {
-    const coordinate = comptime darts.Coordinate.init(0.0, -1.0);
-    const expected = 10;
-    const actual = comptime coordinate.score();
+    const expected: usize = 10;
+    const coordinate = darts.Coordinate.init(0.0, -1.0);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "exactly on center" {
-    const coordinate = comptime darts.Coordinate.init(0.0, 0.0);
-    const expected = 10;
-    const actual = comptime coordinate.score();
+    const expected: usize = 10;
+    const coordinate = darts.Coordinate.init(0.0, 0.0);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "near the center" {
-    const coordinate = comptime darts.Coordinate.init(-0.1, -0.1);
-    const expected = 10;
-    const actual = comptime coordinate.score();
+    const expected: usize = 10;
+    const coordinate = darts.Coordinate.init(-0.1, -0.1);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "just within the inner circle" {
-    const coordinate = comptime darts.Coordinate.init(0.7, 0.7);
-    const expected = 10;
-    const actual = comptime coordinate.score();
+    const expected: usize = 10;
+    const coordinate = darts.Coordinate.init(0.7, 0.7);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "just outside the inner circle" {
-    const coordinate = comptime darts.Coordinate.init(0.8, -0.8);
-    const expected = 5;
-    const actual = comptime coordinate.score();
+    const expected: usize = 5;
+    const coordinate = darts.Coordinate.init(0.8, -0.8);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "just within the middle circle" {
-    const coordinate = comptime darts.Coordinate.init(3.5, -3.5);
-    const expected = 5;
-    const actual = comptime coordinate.score();
+    const expected: usize = 5;
+    const coordinate = darts.Coordinate.init(3.5, -3.5);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "just outside the middle circle" {
-    const coordinate = comptime darts.Coordinate.init(-3.6, -3.6);
-    const expected = 1;
-    const actual = comptime coordinate.score();
+    const expected: usize = 1;
+    const coordinate = darts.Coordinate.init(-3.6, -3.6);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "just within the outer circle" {
-    const coordinate = comptime darts.Coordinate.init(-7.0, 7.0);
-    const expected = 1;
-    const actual = comptime coordinate.score();
+    const expected: usize = 1;
+    const coordinate = darts.Coordinate.init(-7.0, 7.0);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "just outside the outer circle" {
-    const coordinate = comptime darts.Coordinate.init(7.1, -7.1);
-    const expected = 0;
-    const actual = comptime coordinate.score();
+    const expected: usize = 0;
+    const coordinate = darts.Coordinate.init(7.1, -7.1);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
 
 test "asymmetric position between the inner and middle circles" {
-    const coordinate = comptime darts.Coordinate.init(0.5, -4.0);
-    const expected = 5;
-    const actual = comptime coordinate.score();
+    const expected: usize = 5;
+    const coordinate = darts.Coordinate.init(0.5, -4.0);
+    const actual = coordinate.score();
     try testing.expectEqual(expected, actual);
 }
