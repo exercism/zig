3c81860d9b9a24ded31b1e4f2666863d3d75bb41
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-03 11:25:08 +0100

    exercises(space-age): redesign to use only one method (#199)
    Closes: #156

--- exercises/practice/space-age/space_age.zig
+++ exercises/practice/space-age/space_age.zig
@@ -1,61 +1,9 @@
-// An enum should be helpful for this exercise.
+pub const Planet = enum {
+    mercury,
 
-pub const SpaceAge = struct {
-    // This struct, as well as its fields and methods, needs to be
-    // implemented.
-
-    pub fn init(seconds: isize) SpaceAge {
+    pub fn age(self: Planet, seconds: usize) f64 {
+        _ = self;
         _ = seconds;
-        @compileError("please implement the init method");
-    }
-
-    fn getOrbitalPeriodInSecondsFromEarthYearsOf(planet: Planet) f64 {
-        _ = planet;
-        @compileError("please implement the getOrbitalPeriodInSecondsFromEarthYearsOf method");
-    }
-
-    fn getOrbitalPeriodInEarthYearsOf(planet: Planet) f64 {
-        _ = planet;
-        @compileError("please implement the getOrbitalPeriodInEarthYearsOf method");
-    }
-
-    pub fn onMercury(self: SpaceAge) f64 {
-        _ = self;
-        @compileError("please implement the onMercury method");
-    }
-
-    pub fn onVenus(self: SpaceAge) f64 {
-        _ = self;
-        @compileError("please implement the onVenus method");
-    }
-
-    pub fn onEarth(self: SpaceAge) f64 {
-        _ = self;
-        @compileError("please implement the onEarth method");
-    }
-
-    pub fn onMars(self: SpaceAge) f64 {
-        _ = self;
-        @compileError("please implement the onMars method");
-    }
-
-    pub fn onJupiter(self: SpaceAge) f64 {
-        _ = self;
-        @compileError("please implement the onJupiter method");
-    }
-
-    pub fn onSaturn(self: SpaceAge) f64 {
-        _ = self;
-        @compileError("please implement the onSaturn method");
-    }
-
-    pub fn onUranus(self: SpaceAge) f64 {
-        _ = self;
-        @compileError("please implement the onUranus method");
-    }
-
-    pub fn onNeptune(self: SpaceAge) f64 {
-        _ = self;
-        @compileError("please implement the onNeptune method");
+        @compileError("please implement the age method");
     }
 };
--- exercises/practice/space-age/test_space_age.zig
+++ exercises/practice/space-age/test_space_age.zig
@@ -1,61 +1,43 @@
 const std = @import("std");
 const expectApproxEqAbs = std.testing.expectApproxEqAbs;
-const tolerance = 0.02;
 
 const space_age = @import("space_age.zig");
+const Planet = space_age.Planet;
+
+fn testAge(planet: Planet, seconds: usize, expected_age_in_earth_years: f64) !void {
+    const tolerance = 0.02;
+    const actual = planet.age(seconds);
+    try expectApproxEqAbs(expected_age_in_earth_years, actual, tolerance);
+}
 
 test "age on earth" {
-    const sp = comptime space_age.SpaceAge.init(1_000_000_000);
-    const expected: f64 = 31.69;
-    const actual = comptime sp.onEarth();
-    try expectApproxEqAbs(expected, actual, tolerance);
+    try testAge(Planet.earth, 1_000_000_000, 31.69);
 }
 
 test "age on mercury" {
-    const sp = comptime space_age.SpaceAge.init(2_134_835_688);
-    const expected: f64 = 280.88;
-    const actual = comptime sp.onMercury();
-    try expectApproxEqAbs(expected, actual, tolerance);
+    try testAge(Planet.mercury, 2_134_835_688, 280.88);
 }
 
 test "age on venus" {
-    const sp = comptime space_age.SpaceAge.init(189_839_836);
-    const expected: f64 = 9.78;
-    const actual = comptime sp.onVenus();
-    try expectApproxEqAbs(expected, actual, tolerance);
+    try testAge(Planet.venus, 189_839_836, 9.78);
 }
 
 test "age on mars" {
-    const sp = comptime space_age.SpaceAge.init(2_129_871_239);
-    const expected: f64 = 35.88;
-    const actual = comptime sp.onMars();
-    try expectApproxEqAbs(expected, actual, tolerance);
+    try testAge(Planet.mars, 2_129_871_239, 35.88);
 }
 
 test "age on jupiter" {
-    const sp = comptime space_age.SpaceAge.init(901_876_382);
-    const expected: f64 = 2.41;
-    const actual = comptime sp.onJupiter();
-    try expectApproxEqAbs(expected, actual, tolerance);
+    try testAge(Planet.jupiter, 901_876_382, 2.41);
 }
 
 test "age on saturn" {
-    const sp = comptime space_age.SpaceAge.init(2_000_000_000);
-    const expected: f64 = 2.15;
-    const actual = comptime sp.onSaturn();
-    try expectApproxEqAbs(expected, actual, tolerance);
+    try testAge(Planet.saturn, 2_000_000_000, 2.15);
 }
 
 test "age on uranus" {
-    const sp = comptime space_age.SpaceAge.init(1_210_123_456);
-    const expected: f64 = 0.46;
-    const actual = comptime sp.onUranus();
-    try expectApproxEqAbs(expected, actual, tolerance);
+    try testAge(Planet.uranus, 1_210_123_456, 0.46);
 }
 
 test "age on neptune" {
-    const sp = comptime space_age.SpaceAge.init(1_821_023_456);
-    const expected: f64 = 0.35;
-    const actual = comptime sp.onNeptune();
-    try expectApproxEqAbs(expected, actual, tolerance);
+    try testAge(Planet.neptune, 1_821_023_456, 0.35);
 }
