bc1a1c74efa093e40c3d4f624856969436e2a550
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-10 17:30:01 +0100

    exercises(sieve): example: automatically adapt to sieve limit
    Automatically adjust the array size and branch quota at compile time.
    
    Previously, we needed to specify both. For example, we specified that we
    needed an array of length at least 168 in order to store the primes up
    to 1000. That required prior knowledge, and would make it cumbersome to
    change the sieve limit.

--- exercises/practice/sieve/.meta/example.zig
+++ exercises/practice/sieve/.meta/example.zig
@@ -24,26 +24,29 @@ fn eratosthenes(comptime n: u32) []const bool {
 }
 
 /// Returns the prime numbers up to `limit`.
-fn genPrimes(comptime limit: u32, comptime count: u32) []const u32 {
+fn genPrimes(comptime limit: u32) []const u32 {
+    @setEvalBranchQuota(limit * 3);
     const bools = comptime eratosthenes(limit);
-    var result: [count]u32 = undefined;
+    // See https://en.wikipedia.org/wiki/Prime-counting_function#Inequalities
+    const upper_bound_count = @ceil(1.25506 * @as(f64, limit) / @log(@as(f64, limit)));
+    var result: [upper_bound_count]u32 = undefined;
     var j = 0;
-    @setEvalBranchQuota(2000);
     for (bools) |b, i| {
         if (b) {
             result[j] = i;
             j += 1;
         }
     }
-    return &result;
+    return result[0..j];
 }
 
 pub fn primes(buffer: []u32, comptime limit: u32) []const u32 {
-    comptime assert(limit <= 1000);
-    const primes_under_1000 = comptime genPrimes(1000, 168);
+    const sieve_limit = 1000;
+    comptime assert(limit <= sieve_limit);
+    const p = comptime genPrimes(sieve_limit); // The primes up to the sieve limit.
     _ = buffer;
-    if (limit >= primes_under_1000[primes_under_1000.len - 1]) return primes_under_1000;
+    if (limit >= p[p.len - 1]) return p;
     var i: usize = 0;
-    while (primes_under_1000[i] <= limit) : (i += 1) {}
-    return primes_under_1000[0..i];
+    while (p[i] <= limit) : (i += 1) {}
+    return p[0..i];
 }
