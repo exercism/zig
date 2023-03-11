1355ff9b04cee653977708dc98a8ea0b710df659
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-11 13:00:01 +0100

    exercises(linked-list): stub: replace ??? with void
    The repo CI runs `zig fmt` on all Zig files, and so `zig fmt` must exit
    with an error code of 0 for every stub.

--- exercises/practice/linked-list/linked_list.zig
+++ exercises/practice/linked-list/linked_list.zig
@@ -1,26 +1,26 @@
 pub fn LinkedList(comptime T: type) type {
     _ = T;
     return struct {
-        // Please implement the Node struct.
+        // Please implement the Node struct (replacing each `void`).
         pub const Node = struct {
-            prev: ???,
-            next: ???,
-            data: ???,
+            prev: void,
+            next: void,
+            data: void,
         };
 
-        // Please implement the fields of the linked list.
-        first: ???,
-        last: ???,
-        len: ???,
+        // Please implement the fields of the linked list (replacing each `void`).
+        first: void,
+        last: void,
+        len: void,
 
         // Please implement the below methods.
         // The `pop` and `shift` methods should return an optional pointer to a Node.
 
         pub fn push() void {}
 
-        pub fn pop() ??? {}
+        pub fn pop() void {}
 
-        pub fn shift() ??? {}
+        pub fn shift() void {}
 
         pub fn unshift() void {}
 
