9447578368f2b6321550eed163f286845cefd084
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-11 12:50:01 +0100

    exercises(linked-list): stub: be a bit more generous
    It's tricky to find the right balance here.

--- exercises/practice/linked-list/linked_list.zig
+++ exercises/practice/linked-list/linked_list.zig
@@ -2,19 +2,29 @@ pub fn LinkedList(comptime T: type) type {
     _ = T;
     return struct {
         // Please implement the Node struct.
-        pub const Node = struct {};
+        pub const Node = struct {
+            prev: ???,
+            next: ???,
+            data: ???,
+        };
 
-        // Please implement the fields of the linked list, and the below methods.
-        // You will need to change some of the below return types.
+        // Please implement the fields of the linked list.
+        first: ???,
+        last: ???,
+        len: ???,
+
+        // Please implement the below methods.
+        // The `pop` and `shift` methods should return an optional pointer to a Node.
 
         pub fn push() void {}
 
-        pub fn pop() void {}
+        pub fn pop() ??? {}
 
-        pub fn shift() void {}
+        pub fn shift() ??? {}
 
         pub fn unshift() void {}
 
+        // The `delete` method must only modify the list when it contains the given node.
         pub fn delete() void {}
     };
 }
