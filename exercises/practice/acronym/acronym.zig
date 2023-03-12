646059e7721de59d8823073639469e2ba0dccea6
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-12 12:20:01 +0100

    exercises(acronym): stub: remove unused ascii import
    The other stubs don't do this, and it may produce a compile error in the
    future [1].
    
    It's also fine to implement this exercise without using `std.ascii`.
    
    [1] https://www.github.com/ziglang/zig/issues/335

--- exercises/practice/acronym/acronym.zig
+++ exercises/practice/acronym/acronym.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const mem = std.mem;
-const ascii = std.ascii;
 
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
     _ = allocator;
