26af980edc446dcd5da0b8b52c21c4739b338111
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-15 10:18:58 +0100

    exercises(binary-search): stub: make buffer not an optional (#255)
    The tests do not have a test case for a null `buffer`.
    
    This commit is similar to the previous commit, but is user-facing.
    
    Refs: #229

--- exercises/practice/binary-search/binary_search.zig
+++ exercises/practice/binary-search/binary_search.zig
@@ -1,6 +1,6 @@
 // Take a look at the tests, you might have to change the function arguments
 
-pub fn binarySearch(target: usize, buffer: ?[]const usize) SearchError!usize {
+pub fn binarySearch(target: usize, buffer: []const usize) SearchError!usize {
     _ = target;
     _ = buffer;
     @compileError("please implement the binarySearch function");
