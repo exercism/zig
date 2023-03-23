a0281a21e0d56ce8d2ae92577ba9c9431eb61495
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-23 21:06:02 +0100

    exercises(sum-of-multiples): tests: try to improve readability

--- exercises/practice/sum-of-multiples/test_sum_of_multiples.zig
+++ exercises/practice/sum-of-multiples/test_sum_of_multiples.zig
@@ -5,96 +5,128 @@ const sum = @import("sum_of_multiples.zig").sum;
 
 test "no multiples within limit" {
     const expected: usize = 0;
-    const actual = try sum(testing.allocator, &[_]usize{ 3, 5 }, 1);
+    const factors = [_]usize{ 3, 5 };
+    const limit = 1;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "one factor has multiples within limit" {
     const expected: usize = 3;
-    const actual = try sum(testing.allocator, &[_]usize{ 3, 5 }, 4);
+    const factors = [_]usize{ 3, 5 };
+    const limit = 4;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "more than one multiple within limit" {
     const expected: usize = 9;
-    const actual = try sum(testing.allocator, &[_]usize{3}, 7);
+    const factors = [_]usize{3};
+    const limit = 7;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "more than one factor with multiples within limit" {
     const expected: usize = 23;
-    const actual = try sum(testing.allocator, &[_]usize{ 3, 5 }, 10);
+    const factors = [_]usize{ 3, 5 };
+    const limit = 10;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "each multiple is only counted once" {
     const expected: usize = 2318;
-    const actual = try sum(testing.allocator, &[_]usize{ 3, 5 }, 100);
+    const factors = [_]usize{ 3, 5 };
+    const limit = 100;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "a much larger limit" {
     const expected: usize = 233_168;
-    const actual = try sum(testing.allocator, &[_]usize{ 3, 5 }, 1000);
+    const factors = [_]usize{ 3, 5 };
+    const limit = 1000;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "three factors" {
     const expected: usize = 51;
-    const actual = try sum(testing.allocator, &[_]usize{ 7, 13, 17 }, 20);
+    const factors = [_]usize{ 7, 13, 17 };
+    const limit = 20;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "factors not relatively prime" {
     const expected: usize = 30;
-    const actual = try sum(testing.allocator, &[_]usize{ 4, 6 }, 15);
+    const factors = [_]usize{ 4, 6 };
+    const limit = 15;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "some pairs of factors relatively prime and some not" {
     const expected: usize = 4419;
-    const actual = try sum(testing.allocator, &[_]usize{ 5, 6, 8 }, 150);
+    const factors = [_]usize{ 5, 6, 8 };
+    const limit = 150;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "one factor is a multiple of another" {
     const expected: usize = 275;
-    const actual = try sum(testing.allocator, &[_]usize{ 5, 25 }, 51);
+    const factors = [_]usize{ 5, 25 };
+    const limit = 51;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "much larger factors" {
     const expected: usize = 2_203_160;
-    const actual = try sum(testing.allocator, &[_]usize{ 43, 47 }, 10_000);
+    const factors = [_]usize{ 43, 47 };
+    const limit = 10_000;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "all numbers are multiples of 1" {
     const expected: usize = 4_950;
-    const actual = try sum(testing.allocator, &[_]usize{1}, 100);
+    const factors = [_]usize{1};
+    const limit = 100;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "no factors means an empty sum" {
     const expected: usize = 0;
-    const actual = try sum(testing.allocator, &[_]usize{}, 10_000);
+    const factors = [_]usize{};
+    const limit = 10_000;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "the only multiple of 0 is 0" {
     const expected: usize = 0;
-    const actual = try sum(testing.allocator, &[_]usize{0}, 1);
+    const factors = [_]usize{0};
+    const limit = 1;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "the factor 0 does not affect the sum of multiples of other factors" {
     const expected: usize = 3;
-    const actual = try sum(testing.allocator, &[_]usize{ 3, 0 }, 4);
+    const factors = [_]usize{ 3, 0 };
+    const limit = 4;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
 
 test "solutions using include-exclude must extend to cardinality greater than 3" {
     const expected: usize = 39_614_537;
-    const actual = try sum(testing.allocator, &[_]usize{ 2, 3, 5, 7, 11 }, 10_000);
+    const factors = [_]usize{ 2, 3, 5, 7, 11 };
+    const limit = 10_000;
+    const actual = try sum(testing.allocator, &factors, limit);
     try testing.expectEqual(expected, actual);
 }
