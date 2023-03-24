4cbfa17d4731e32e9e1eb39cf88f8a7110538bcf
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-24 20:00:01 +0100

    exercises(yacht): implement

---
+++ exercises/practice/yacht/.meta/example.zig
@@ -0,0 +1,78 @@
+const std = @import("std");
+
+pub const Category = enum(u4) {
+    ones,
+    twos,
+    threes,
+    fours,
+    fives,
+    sixes,
+    full_house,
+    four_of_a_kind,
+    little_straight,
+    big_straight,
+    choice,
+    yacht,
+};
+
+const DiceInt = u3;
+const Dice = [5]DiceInt;
+
+/// Returns the sum of the items in `dice` that equal `n`.
+fn sumOnly(dice: Dice, n: @typeInfo(Category).Enum.tag_type) usize {
+    var result: usize = 0;
+    for (dice) |d| {
+        if (d == n) result += n;
+    }
+    return result;
+}
+
+fn sort(dice: Dice) Dice {
+    var d = dice;
+    std.sort.sort(DiceInt, &d, {}, std.sort.asc(DiceInt));
+    return d;
+}
+
+fn sum(dice: Dice) usize {
+    var result: usize = 0;
+    for (dice) |d| result += d;
+    return result;
+}
+
+fn scoreFullHouse(dice: Dice) usize {
+    const d = sort(dice);
+    const cond =
+        (d[0] == d[1] and d[1] == d[2] and d[2] != d[3] and d[3] == d[4]) or
+        (d[0] == d[1] and d[1] != d[2] and d[2] == d[3] and d[3] == d[4]);
+    return if (cond) sum(d) else 0;
+}
+
+fn scoreFourOfAKind(dice: Dice) usize {
+    const d = sort(dice);
+    if (d[1] == d[2] and d[2] == d[3]) {
+        if (d[0] == d[1]) return @as(usize, d[0]) * 4;
+        if (d[3] == d[4]) return @as(usize, d[1]) * 4;
+    }
+    return 0;
+}
+
+fn scoreStraight(dice: Dice, straight: Dice) usize {
+    const d = sort(dice);
+    return if (std.mem.eql(DiceInt, &d, &straight)) 30 else 0;
+}
+
+fn scoreYacht(d: Dice) usize {
+    return if (d[0] == d[1] and d[1] == d[2] and d[2] == d[3] and d[3] == d[4]) 50 else 0;
+}
+
+pub fn score(dice: Dice, category: Category) usize {
+    return switch (category) {
+        .ones, .twos, .threes, .fours, .fives, .sixes => sumOnly(dice, @enumToInt(category) + 1),
+        .full_house => scoreFullHouse(dice),
+        .four_of_a_kind => scoreFourOfAKind(dice),
+        .little_straight => scoreStraight(dice, Dice{ 1, 2, 3, 4, 5 }),
+        .big_straight => scoreStraight(dice, Dice{ 2, 3, 4, 5, 6 }),
+        .choice => sum(dice),
+        .yacht => scoreYacht(dice),
+    };
+}
