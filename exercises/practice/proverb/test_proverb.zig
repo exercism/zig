835eb1e942cef565ebd6795a0b3b494b0b74a002
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-26 20:00:02 +0200

    exercises(proverb): test: simplify

--- exercises/practice/proverb/test_proverb.zig
+++ exercises/practice/proverb/test_proverb.zig
@@ -6,11 +6,11 @@ const proverb = @import("proverb.zig");
 test "zero pieces" {
     const input = [_][]const u8{};
 
-    const expected = &input;
+    const expected = input;
 
     const actual = try proverb.recite(testing.allocator, &input);
 
-    try testing.expectEqualSlices([]const u8, expected, actual);
+    try testing.expectEqualSlices([]const u8, &expected, actual);
 
     // The free here doesn't actually free memory since a zero sized heap
     // allocation doesn't actually point to anything important. So the free
@@ -20,11 +20,11 @@ test "zero pieces" {
 }
 
 test "one piece" {
-    const input1 = "nail".*;
-    const input = [_][]const u8{&input1};
+    const input1 = "nail";
+    const input = [_][]const u8{input1};
 
-    const expected1 = "And all for the want of a nail.\n".*;
-    const expected = [_][]const u8{&expected1};
+    const expected1 = "And all for the want of a nail.\n";
+    const expected = [_][]const u8{expected1};
 
     const actual = try proverb.recite(testing.allocator, &input);
 
@@ -39,13 +39,13 @@ test "one piece" {
 }
 
 test "two pieces" {
-    const input1 = "nail".*;
-    const input2 = "shoe".*;
-    const input = [_][]const u8{ &input1, &input2 };
+    const input1 = "nail";
+    const input2 = "shoe";
+    const input = [_][]const u8{ input1, input2 };
 
-    const expected1 = "For want of a nail the shoe was lost.\n".*;
-    const expected2 = "And all for the want of a nail.\n".*;
-    const expected = [_][]const u8{ &expected1, &expected2 };
+    const expected1 = "For want of a nail the shoe was lost.\n";
+    const expected2 = "And all for the want of a nail.\n";
+    const expected = [_][]const u8{ expected1, expected2 };
 
     const actual = try proverb.recite(testing.allocator, &input);
 
@@ -60,15 +60,15 @@ test "two pieces" {
 }
 
 test "three pieces" {
-    const input1 = "nail".*;
-    const input2 = "shoe".*;
-    const input3 = "horse".*;
-    const input = [_][]const u8{ &input1, &input2, &input3 };
+    const input1 = "nail";
+    const input2 = "shoe";
+    const input3 = "horse";
+    const input = [_][]const u8{ input1, input2, input3 };
 
-    const expected1 = "For want of a nail the shoe was lost.\n".*;
-    const expected2 = "For want of a shoe the horse was lost.\n".*;
-    const expected3 = "And all for the want of a nail.\n".*;
-    const expected = [_][]const u8{ &expected1, &expected2, &expected3 };
+    const expected1 = "For want of a nail the shoe was lost.\n";
+    const expected2 = "For want of a shoe the horse was lost.\n";
+    const expected3 = "And all for the want of a nail.\n";
+    const expected = [_][]const u8{ expected1, expected2, expected3 };
 
     const actual = try proverb.recite(testing.allocator, &input);
 
@@ -83,23 +83,23 @@ test "three pieces" {
 }
 
 test "full proverb" {
-    const input1 = "nail".*;
-    const input2 = "shoe".*;
-    const input3 = "horse".*;
-    const input4 = "rider".*;
-    const input5 = "message".*;
-    const input6 = "battle".*;
-    const input7 = "kingdom".*;
-    const input = [_][]const u8{ &input1, &input2, &input3, &input4, &input5, &input6, &input7 };
+    const input1 = "nail";
+    const input2 = "shoe";
+    const input3 = "horse";
+    const input4 = "rider";
+    const input5 = "message";
+    const input6 = "battle";
+    const input7 = "kingdom";
+    const input = [_][]const u8{ input1, input2, input3, input4, input5, input6, input7 };
 
-    const expected1 = "For want of a nail the shoe was lost.\n".*;
-    const expected2 = "For want of a shoe the horse was lost.\n".*;
-    const expected3 = "For want of a horse the rider was lost.\n".*;
-    const expected4 = "For want of a rider the message was lost.\n".*;
-    const expected5 = "For want of a message the battle was lost.\n".*;
-    const expected6 = "For want of a battle the kingdom was lost.\n".*;
-    const expected7 = "And all for the want of a nail.\n".*;
-    const expected = [_][]const u8{ &expected1, &expected2, &expected3, &expected4, &expected5, &expected6, &expected7 };
+    const expected1 = "For want of a nail the shoe was lost.\n";
+    const expected2 = "For want of a shoe the horse was lost.\n";
+    const expected3 = "For want of a horse the rider was lost.\n";
+    const expected4 = "For want of a rider the message was lost.\n";
+    const expected5 = "For want of a message the battle was lost.\n";
+    const expected6 = "For want of a battle the kingdom was lost.\n";
+    const expected7 = "And all for the want of a nail.\n";
+    const expected = [_][]const u8{ expected1, expected2, expected3, expected4, expected5, expected6, expected7 };
 
     const actual = try proverb.recite(testing.allocator, &input);
 
@@ -114,17 +114,17 @@ test "full proverb" {
 }
 
 test "four pieces modernized" {
-    const input1 = "pin".*;
-    const input2 = "gun".*;
-    const input3 = "soldier".*;
-    const input4 = "battle".*;
-    const input = [_][]const u8{ &input1, &input2, &input3, &input4 };
+    const input1 = "pin";
+    const input2 = "gun";
+    const input3 = "soldier";
+    const input4 = "battle";
+    const input = [_][]const u8{ input1, input2, input3, input4 };
 
-    const expected1 = "For want of a pin the gun was lost.\n".*;
-    const expected2 = "For want of a gun the soldier was lost.\n".*;
-    const expected3 = "For want of a soldier the battle was lost.\n".*;
-    const expected4 = "And all for the want of a pin.\n".*;
-    const expected = [_][]const u8{ &expected1, &expected2, &expected3, &expected4 };
+    const expected1 = "For want of a pin the gun was lost.\n";
+    const expected2 = "For want of a gun the soldier was lost.\n";
+    const expected3 = "For want of a soldier the battle was lost.\n";
+    const expected4 = "And all for the want of a pin.\n";
+    const expected = [_][]const u8{ expected1, expected2, expected3, expected4 };
 
     const actual = try proverb.recite(testing.allocator, &input);
 
