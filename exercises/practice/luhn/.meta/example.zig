67eb0fcc6231f208680017f70c4587385b1a1d4c
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-29 14:31:05 +0200

    exercises(luhn): example: rename variable
    Also more consistent with the isbn-verifier example.

--- exercises/practice/luhn/.meta/example.zig
+++ exercises/practice/luhn/.meta/example.zig
@@ -1,7 +1,7 @@
 pub fn isValid(s: []const u8) bool {
     const lookup = [10]u8{ 0, 2, 4, 6, 8, 1, 3, 5, 7, 9 };
     var sum: usize = 0;
-    var n_digits: usize = 0;
+    var digit_count: usize = 0;
     var i = s.len;
 
     while (i > 0) {
@@ -10,13 +10,13 @@ pub fn isValid(s: []const u8) bool {
         switch (c) {
             '0'...'9' => {
                 const digit = c - '0';
-                sum += if (n_digits % 2 == 0) digit else lookup[digit];
-                n_digits += 1;
+                sum += if (digit_count % 2 == 0) digit else lookup[digit];
+                digit_count += 1;
             },
             ' ' => continue,
             else => return false,
         }
     }
 
-    return (sum % 10 == 0) and n_digits > 1;
+    return (sum % 10 == 0) and digit_count > 1;
 }
