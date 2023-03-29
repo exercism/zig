3b4f2c0403d51ad6daa3368296029a0bf8935213
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-29 14:31:03 +0200

    exercises(luhn): example: remove comptime lookup creation

--- exercises/practice/luhn/.meta/example.zig
+++ exercises/practice/luhn/.meta/example.zig
@@ -1,17 +1,5 @@
-const std = @import("std");
-
-fn createLookup() [10]u8 {
-    var result: [10]u8 = undefined;
-    for (result) |*item, i| {
-        const d = 2 * @intCast(u8, i);
-        item.* = if (d > 9) d - 9 else d;
-    }
-    return result;
-}
-
 pub fn isValid(s: []const u8) bool {
-    const lookup = comptime createLookup();
-    comptime std.debug.assert(std.mem.eql(u8, &lookup, &[10]u8{ 0, 2, 4, 6, 8, 1, 3, 5, 7, 9 }));
+    const lookup = [10]u8{ 0, 2, 4, 6, 8, 1, 3, 5, 7, 9 };
     var sum: usize = 0;
     var n_digits: usize = 0;
     var i = s.len;
