20e7848d7be8df5bd7be96a9d4e4f451d41d9aca
ee7 <45465154+ee7@users.noreply.github.com>
2023-04-02 12:15:04 +0200

    exercises(matching-brackets): example: show ArrayList solution

--- exercises/practice/matching-brackets/.meta/example.zig
+++ exercises/practice/matching-brackets/.meta/example.zig
@@ -2,25 +2,19 @@ const std = @import("std");
 const mem = std.mem;
 
 /// Returns whether the characters `(`, `[`, and `{` are matched and correctly nested in `s`.
-/// However, returns `false` if a stack of unmatched characters would exceed a length of 10.
-pub fn isBalanced(allocator: mem.Allocator, s: []const u8) !bool {
-    _ = allocator;
-    var stack: [10]u8 = undefined;
-    var i: usize = 0;
+pub fn isBalanced(allocator: mem.Allocator, s: []const u8) mem.Allocator.Error!bool {
+    var stack = std.ArrayList(u8).init(allocator);
+    defer stack.deinit();
+
     for (s) |c| {
         switch (c) {
-            '(', '[', '{' => {
-                if (i == stack.len) return false;
-                stack[i] = c;
-                i += 1;
-                continue;
-            },
-            ')' => if (i == 0 or stack[i - 1] != '(') return false,
-            ']' => if (i == 0 or stack[i - 1] != '[') return false,
-            '}' => if (i == 0 or stack[i - 1] != '{') return false,
+            '(', '[', '{' => try stack.append(c),
+            ')' => if (stack.items.len == 0 or stack.pop() != '(') return false,
+            ']' => if (stack.items.len == 0 or stack.pop() != '[') return false,
+            '}' => if (stack.items.len == 0 or stack.pop() != '{') return false,
             else => continue,
         }
-        i -= 1;
     }
-    return i == 0;
+
+    return stack.items.len == 0;
 }
