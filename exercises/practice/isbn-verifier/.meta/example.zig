17317773a25681bbf357a53a91fe46af68dce4b8
Roman Frołow <rofrol@gmail.com>
2023-04-07 16:29:16 +0200

    Shorter isbn-verifier example

--- exercises/practice/isbn-verifier/.meta/example.zig
+++ exercises/practice/isbn-verifier/.meta/example.zig
@@ -1,21 +1,19 @@
 pub fn isValidIsbn10(s: []const u8) bool {
     if (s.len < 10) return false;
-    var digit_count: usize = 0;
+    var n: usize = 0;
     var sum: usize = 0;
-    var n: usize = 10;
 
     for (s) |c, i| {
         switch (c) {
-            '0'...'9' => sum += (c - '0') * n,
+            '0'...'9' => sum += (c - '0') * (10 - n),
             'X' => if (i == s.len - 1) {
-                sum += 10 * n;
+                sum += 10 * (10 - n);
             } else return false,
             '-' => continue,
             else => return false,
         }
-        if (n > 0) n -= 1;
-        digit_count += 1;
+        n += 1;
     }
 
-    return (sum % 11 == 0) and digit_count == 10;
+    return (sum % 11 == 0) and n == 10;
 }
