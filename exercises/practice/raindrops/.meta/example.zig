a5d006617fd62232518b6e89eee30f784438661e
ee7 <45465154+ee7@users.noreply.github.com>
2023-04-09 20:00:01 +0200

    exercises(raindrops): example: simplify return

--- exercises/practice/raindrops/.meta/example.zig
+++ exercises/practice/raindrops/.meta/example.zig
@@ -5,6 +5,5 @@ pub fn convert(buf: []u8, comptime n: usize) []const u8 {
     const plang = if (n % 5 == 0) "Plang" else "";
     const plong = if (n % 7 == 0) "Plong" else "";
     const result = pling ++ plang ++ plong;
-    if (result.len == 0) return std.fmt.bufPrint(buf, "{}", .{n}) catch unreachable;
-    return result;
+    return if (result.len > 0) result else std.fmt.bufPrint(buf, "{}", .{n}) catch unreachable;
 }
