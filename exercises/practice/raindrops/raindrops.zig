95bc05718799764035d7b56683d48f1e6b7fa1fa
ee7 <45465154+ee7@users.noreply.github.com>
2023-04-09 20:00:02 +0200

    exercises(raindrops): example, stub: rename `buf` to `buffer`
    Use the variable name that we use in the tests, and other exercises.

--- exercises/practice/raindrops/raindrops.zig
+++ exercises/practice/raindrops/raindrops.zig
@@ -1,5 +1,5 @@
-pub fn convert(buf: []u8, n: usize) []const u8 {
-    _ = buf;
+pub fn convert(buffer: []u8, n: usize) []const u8 {
+    _ = buffer;
     _ = n;
     @compileError("please implement the convert function");
 }
