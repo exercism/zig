9219ac644d4752d1cbb65155285c84a37f7e0fc8
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-03 20:00:01 +0200

    exercises(sum-of-multiples): example: rename intset to multiples

--- exercises/practice/sum-of-multiples/.meta/example.zig
+++ exercises/practice/sum-of-multiples/.meta/example.zig
@@ -2,16 +2,16 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
-    var intset = std.AutoHashMap(u64, void).init(allocator);
-    defer intset.deinit();
+    var multiples = std.AutoHashMap(u64, void).init(allocator);
+    defer multiples.deinit();
 
     var result: u64 = 0;
     for (factors) |f| {
         if (f != 0) {
             var n: u64 = f;
             while (n < limit) {
-                if (!intset.contains(n)) {
-                    try intset.putNoClobber(n, {});
+                if (!multiples.contains(n)) {
+                    try multiples.putNoClobber(n, {});
                     result += n;
                 }
                 n += f;
