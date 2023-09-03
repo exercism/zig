330db9f807ae051b67ac8c22bb87253a4b2cf213
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-03 20:00:03 +0200

    exercises(sum-of-multiples): example: specify error

--- exercises/practice/sum-of-multiples/.meta/example.zig
+++ exercises/practice/sum-of-multiples/.meta/example.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const mem = std.mem;
 
-pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
+pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) mem.Allocator.Error!u64 {
     var multiples = std.AutoHashMap(u64, void).init(allocator);
     defer multiples.deinit();
 
