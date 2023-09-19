716836913210a8f56f86b02dc254a0d0e9447bca
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-19 13:00:04 +0200

    exercises(secret-handshake): example: oneline ifs

--- exercises/practice/secret-handshake/.meta/example.zig
+++ exercises/practice/secret-handshake/.meta/example.zig
@@ -11,21 +11,11 @@ pub const Signal = enum(u2) {
 pub fn calculateHandshake(allocator: mem.Allocator, number: u5) mem.Allocator.Error![]const Signal {
     var list = std.ArrayList(Signal).init(allocator);
     errdefer list.deinit();
-    if (isFlipped(number, 0b1)) {
-        try list.append(Signal.wink);
-    }
-    if (isFlipped(number, 0b10)) {
-        try list.append(Signal.double_blink);
-    }
-    if (isFlipped(number, 0b100)) {
-        try list.append(Signal.close_your_eyes);
-    }
-    if (isFlipped(number, 0b1000)) {
-        try list.append(Signal.jump);
-    }
-    if (isFlipped(number, 0b10000)) {
-        mem.reverse(Signal, list.items);
-    }
+    if (isFlipped(number, 0b1)) try list.append(Signal.wink);
+    if (isFlipped(number, 0b10)) try list.append(Signal.double_blink);
+    if (isFlipped(number, 0b100)) try list.append(Signal.close_your_eyes);
+    if (isFlipped(number, 0b1000)) try list.append(Signal.jump);
+    if (isFlipped(number, 0b10000)) mem.reverse(Signal, list.items);
     return list.toOwnedSlice();
 }
 
