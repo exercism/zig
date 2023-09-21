8576b88d3f8d1d911a392e3932b960fe3c598519
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-21 12:35:04 +0200

    exercises(grains): assert rather than error

--- exercises/practice/grains/.meta/example.zig
+++ exercises/practice/grains/.meta/example.zig
@@ -1,12 +1,8 @@
 const std = @import("std");
 
-pub const ChessboardError = error{IndexOutOfBounds};
-
-pub fn square(index: u7) ChessboardError!u64 {
-    const number_of_chess_squares = 64;
-    if (index > number_of_chess_squares or index == 0) {
-        return ChessboardError.IndexOutOfBounds;
-    }
+/// Asserts that `index` is greater than 0 and less than 65.
+pub fn square(index: u7) u64 {
+    std.debug.assert(index > 0 and index < 65);
     return @as(u64, 1) << @as(u6, @truncate(index - 1));
 }
 
