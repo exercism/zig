34031909216e3c7e415287dd89e2bd9043b1abd4
ee7 <45465154+ee7@users.noreply.github.com>
2023-11-14 19:38:34 +0100

    exercises(darts): example: remove unnecessary Self (#357)
    Use the preferred style [1].
    
    The only other `@This()` in this repo is in the example solution for the
    `linked-list` exercise, where it's appropriate.
    
    [1] https://zig.news/kristoff/dont-self-simple-structs-fj8

--- exercises/practice/darts/.meta/example.zig
+++ exercises/practice/darts/.meta/example.zig
@@ -1,14 +1,12 @@
 pub const Coordinate = struct {
-    const Self = @This();
-
     x: f32,
     y: f32,
 
-    pub fn init(x: f32, y: f32) Self {
+    pub fn init(x: f32, y: f32) Coordinate {
         return .{ .x = x, .y = y };
     }
 
-    pub fn score(self: Self) u32 {
+    pub fn score(self: Coordinate) u32 {
         const n = (self.x * self.x) + (self.y * self.y); // The distance squared.
         return if (n <= 1) 10 else if (n <= 25) 5 else if (n <= 100) 1 else 0;
     }
