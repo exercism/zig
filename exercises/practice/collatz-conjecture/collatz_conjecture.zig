36f95bdff9a2cb4af205994228e511312ca2c5a0
mcheviron <87418909+mcheviron@users.noreply.github.com>
2023-11-16 19:10:26 +0100

    Update collatz_conjecture.zig to contain the errors expected in the tests

--- exercises/practice/collatz-conjecture/collatz_conjecture.zig
+++ exercises/practice/collatz-conjecture/collatz_conjecture.zig
@@ -1,4 +1,6 @@
+pub const ComputationError = error{ IllegalArgument };
+
 pub fn steps(number: usize) anyerror!usize {
     _ = number;
-    @compileError("please implement the steps function");
+    @compileError("please implement the steps function or return aforementioned error");
 }
