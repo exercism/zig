bbee46931672d951e2f72b8a4e857ef83c5d06dd
paulnice <ru.pchel@gmail.com>
2024-05-26 12:04:26 -0700

    Update declaration of the binarySearch function.
    Problem: Previous declaration wasn't compatible with the tests:
    ```
    test_binary_search.zig:10:20: error: expected 2 argument(s), found 3
        const actual = binarySearch(i4, 6, &array);
                       ^~~~~~~~~~~~
    binary_search.zig:3:5: note: function declared here
    ```
    Solution: declare the function properly.
    
    Testing: with existing test cases

--- exercises/practice/binary-search/binary_search.zig
+++ exercises/practice/binary-search/binary_search.zig
@@ -1,6 +1,6 @@
 // Take a look at the tests, you might have to change the function arguments
 
-pub fn binarySearch(target: usize, items: []const usize) ?usize {
+pub fn binarySearch(comptime T: type, target: usize, items: []const T) ?usize {
     _ = target;
     _ = items;
     @compileError("please implement the binarySearch function");
