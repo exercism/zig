f29774ccc026987be8bc5d30aef3edb5dcf95c38
Eric Willigers <keiravillekode@gmail.com>
2024-06-15 10:15:35 +1000

    Add pig-latin exercise

---
+++ exercises/practice/pig-latin/pig_latin.zig
@@ -0,0 +1,5 @@
+pub fn translate(allocator: mem.Allocator, phrase: []const u8) mem.Allocator.Error![]u8 {
+    _ = allocator;
+    _ = phrase;
+    @compileError("please implement the translate function");
+}
