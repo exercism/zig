a74b6519f40a19e433e33a8da811aa2f003db1a6
keiravillekode <keiravillekode@gmail.com>
2025-01-31 05:56:19 +1100

    Add prime-factors exercise (#444)

---
+++ exercises/practice/prime-factors/.meta/example.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const mem = std.mem;
+
+pub fn factors(allocator: mem.Allocator, value: u64) mem.Allocator.Error![]u64 {
+    var list = std.ArrayList(u64).init(allocator);
+    errdefer list.deinit();
+    var number = value;
+    var p: u64 = 2;
+
+    while (number > 1) {
+        if (p * p > number) {
+            p = number;
+        }
+
+        if (number % p == 0) {
+            try list.append(p);
+            number /= p;
+        } else {
+            p += 1;
+        }
+    }
+
+    return list.toOwnedSlice();
+}
