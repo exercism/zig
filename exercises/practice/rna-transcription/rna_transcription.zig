2d2593201f84ce4f48b3b954fadc6867ff603c40
Rob Hand <146272+sinon@users.noreply.github.com>
2025-03-25 11:03:52 +0000

    Add missing std imports to show useful @compileError

--- exercises/practice/rna-transcription/rna_transcription.zig
+++ exercises/practice/rna-transcription/rna_transcription.zig
@@ -1,4 +1,5 @@
-// Import the appropriate standard library and modules
+const std = @import("std");
+const mem = std.mem;
 
 pub fn toRna(allocator: mem.Allocator, dna: []const u8) mem.Allocator.Error![]const u8 {
     _ = allocator;
