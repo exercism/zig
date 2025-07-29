d3362a5ea1cc66ff87dccbf7282fc33215e46fbb
ee7 <45465154+ee7@users.noreply.github.com>
2025-07-29 20:00:01 +0200

    .github, docs, exercises: bump Zig from 0.14.1 to 0.15.0-dev.1254+c9ce1debe

--- exercises/practice/proverb/proverb.zig
+++ exercises/practice/proverb/proverb.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const mem = std.mem;
 const fmt = std.fmt;
 
-pub fn recite(allocator: mem.Allocator, words: []const []const u8) (fmt.AllocPrintError || mem.Allocator.Error)![][]u8 {
+pub fn recite(allocator: mem.Allocator, words: []const []const u8) mem.Allocator.Error![][]u8 {
     _ = allocator;
     _ = words;
     @compileError("please implement the recite function");
