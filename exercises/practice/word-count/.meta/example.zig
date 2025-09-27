7b33a2ac9620af640124f5d4b0d04498b0f0e651
keiravillekode <keiravillekode@gmail.com>
2025-09-28 01:56:55 +1000

    word-count - checkAllAllocationFailures (#490)

--- exercises/practice/word-count/.meta/example.zig
+++ exercises/practice/word-count/.meta/example.zig
@@ -1,44 +1,54 @@
 const std = @import("std");
+const ascii = std.ascii;
 const mem = std.mem;
-const StringMap = std.StringHashMap(u32);
-const Word = std.array_list.Managed(u8);
 
-/// If `self` contains the key `word`, increments its value by 1.
-/// Otherwise, dupes `word` and puts it into `self`, setting its value to 1.
-/// Clears `word`.
-fn incOrPut(self: *StringMap, word: *Word) mem.Allocator.Error!void {
-    const res = try self.getOrPut(word.items);
-    if (res.found_existing) {
-        res.value_ptr.* += 1;
-    } else {
-        const dupe = try self.allocator.dupe(u8, word.items);
-        errdefer self.allocator.free(dupe);
-        res.key_ptr.* = dupe;
-        res.value_ptr.* = 1;
+fn freeKeysAndDeinit(self: *std.StringHashMap(u32)) void {
+    var iter = self.keyIterator();
+    while (iter.next()) |key_ptr| {
+        self.allocator.free(key_ptr.*);
     }
-    word.clearRetainingCapacity();
+    self.deinit();
+}
+
+fn insert(allocator: mem.Allocator, result: *std.StringHashMap(u32), word: []const u8) !void {
+    const key = try allocator.dupe(u8, word);
+    errdefer allocator.free(key);
+    try result.*.put(key, 1);
 }
 
 /// Returns the counts of the words in `s`.
 /// Caller owns the returned memory.
-pub fn countWords(allocator: mem.Allocator, s: []const u8) mem.Allocator.Error!StringMap {
-    var result = StringMap.init(allocator);
-    errdefer result.deinit();
-
-    var word = Word.init(allocator);
-    defer word.deinit();
-
-    for (s, 0..) |c, i| {
-        switch (c) {
-            '0'...'9' => try word.append(c),
-            'A'...'Z' => try word.append('a' + c - 'A'),
-            'a'...'z' => try word.append(c),
-            '\'' => if (word.items.len > 0 and i + 1 < s.len and std.ascii.isAlphabetic(s[i + 1])) {
-                try word.append(c);
-            },
-            else => if (word.items.len > 0) try incOrPut(&result, &word),
-        }
+pub fn countWords(allocator: mem.Allocator, s: []const u8) !std.StringHashMap(u32) {
+    var lower = try allocator.alloc(u8, s.len);
+    defer allocator.free(lower);
+    for (s, 0..) |ch, i| {
+        lower[i] = ascii.toLower(ch);
     }
-    if (word.items.len > 0) try incOrPut(&result, &word);
+    var result = std.StringHashMap(u32).init(allocator);
+    errdefer freeKeysAndDeinit(&result);
+
+    var first: usize = 0;
+    outer: while (first < lower.len) {
+        while (!ascii.isAlphanumeric(lower[first])) {
+            first += 1;
+            if (first == lower.len) {
+                break :outer;
+            }
+        }
+        var last = first + 1;
+        while (last < lower.len and (lower[last] == '\'' or ascii.isAlphanumeric(lower[last]))) {
+            last += 1;
+        }
+        while (lower[last - 1] == '\'') {
+            last -= 1;
+        }
+        const word = lower[first..last];
+        if (result.getEntry(word)) |entry| {
+            entry.value_ptr.* = entry.value_ptr.* + 1;
+        } else {
+            try insert(allocator, &result, word);
+        }
+        first = last + 1;
+    }
     return result;
 }
