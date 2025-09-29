b9f58d0917d8e1057b3eb2c25611746b0c5a5792
Eric Willigers <keiravillekode@gmail.com>
2025-09-29 13:56:48 +1000

    two-fer omits anyerror
    [no important files changed]

--- exercises/practice/two-fer/two_fer.zig
+++ exercises/practice/two-fer/two_fer.zig
@@ -1,4 +1,4 @@
-pub fn twoFer(buffer: []u8, name: ?[]const u8) anyerror![]u8 {
+pub fn twoFer(buffer: []u8, name: ?[]const u8) ![]u8 {
     _ = buffer;
     _ = name;
     @compileError("respond with the appropriate message given a particular name");
