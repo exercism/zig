3aaaf788c6bd037f777791570f1303d0c4ffece8
Eric Willigers <keiravillekode@gmail.com>
2025-10-03 09:11:13 +1000

    bottle-song error reporting
    We allow errors to be returned from recite()
    
    This change breaks existing community solutions.

--- exercises/practice/bottle-song/bottle_song.zig
+++ exercises/practice/bottle-song/bottle_song.zig
@@ -1,4 +1,4 @@
-pub fn recite(buffer: []u8, start_bottles: u32, take_down: u32) []const u8 {
+pub fn recite(buffer: []u8, start_bottles: u32, take_down: u32) ![]const u8 {
     _ = buffer;
     _ = start_bottles;
     _ = take_down;
