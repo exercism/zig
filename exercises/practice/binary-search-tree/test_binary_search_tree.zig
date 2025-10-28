f01f84007587955dfd4f6b58e1ff915215577fc5
Eric Willigers <keiravillekode@gmail.com>
2025-10-29 07:42:36 +1100

    binary-search-tree: checkAllAllocationFailures

--- exercises/practice/binary-search-tree/test_binary_search_tree.zig
+++ exercises/practice/binary-search-tree/test_binary_search_tree.zig
@@ -4,6 +4,18 @@ const testing = std.testing;
 const binary_search_tree = @import("binary_search_tree.zig");
 const Tree = binary_search_tree.Tree;
 
+fn sortedDataTest(allocator: std.mem.Allocator, tree_data: []const i32, expected: []const i32) anyerror!void {
+    var tree = Tree.init(allocator);
+    defer tree.deinit();
+    for (tree_data) |data| {
+        try tree.insert(data);
+    }
+
+    const actual = try tree.sortedData(allocator);
+    defer allocator.free(actual);
+    try testing.expectEqualSlices(i32, expected, actual);
+}
+
 test "data is retained" {
     var tree = Tree.init(testing.allocator);
     defer tree.deinit();
@@ -133,59 +145,67 @@ test "can create complex tree" {
 }
 
 test "can sort data-can sort single number" {
-    var tree = Tree.init(testing.allocator);
-    defer tree.deinit();
-    try tree.insert(2);
+    const tree_data = [_]i32{2};
     const expected = [_]i32{2};
-    const actual = try tree.sortedData();
-    defer testing.allocator.free(actual);
-    try testing.expectEqualSlices(i32, &expected, actual);
+    try std.testing.checkAllAllocationFailures(
+        std.testing.allocator,
+        sortedDataTest,
+        .{ &tree_data, &expected },
+    );
 }
 
 test "can sort data-can sort if second number is smaller than first" {
-    var tree = Tree.init(testing.allocator);
-    defer tree.deinit();
-    try tree.insert(2);
-    try tree.insert(1);
+    const tree_data = [_]i32{ 2, 1 };
     const expected = [_]i32{ 1, 2 };
-    const actual = try tree.sortedData();
-    defer testing.allocator.free(actual);
-    try testing.expectEqualSlices(i32, &expected, actual);
+    try std.testing.checkAllAllocationFailures(
+        std.testing.allocator,
+        sortedDataTest,
+        .{ &tree_data, &expected },
+    );
 }
 
 test "can sort data-can sort if second number is same as first" {
-    var tree = Tree.init(testing.allocator);
-    defer tree.deinit();
-    try tree.insert(2);
-    try tree.insert(2);
+    const tree_data = [_]i32{ 2, 2 };
     const expected = [_]i32{ 2, 2 };
-    const actual = try tree.sortedData();
-    defer testing.allocator.free(actual);
-    try testing.expectEqualSlices(i32, &expected, actual);
+    try std.testing.checkAllAllocationFailures(
+        std.testing.allocator,
+        sortedDataTest,
+        .{ &tree_data, &expected },
+    );
 }
 
 test "can sort data-can sort if second number is greater than first" {
-    var tree = Tree.init(testing.allocator);
-    defer tree.deinit();
-    try tree.insert(2);
-    try tree.insert(3);
+    const tree_data = [_]i32{ 2, 3 };
     const expected = [_]i32{ 2, 3 };
-    const actual = try tree.sortedData();
-    defer testing.allocator.free(actual);
-    try testing.expectEqualSlices(i32, &expected, actual);
+    try std.testing.checkAllAllocationFailures(
+        std.testing.allocator,
+        sortedDataTest,
+        .{ &tree_data, &expected },
+    );
 }
 
 test "can sort data-can sort complex tree" {
+    const tree_data = [_]i32{ 2, 1, 3, 6, 7, 5 };
+    const expected = [_]i32{ 1, 2, 3, 5, 6, 7 };
+    try std.testing.checkAllAllocationFailures(
+        std.testing.allocator,
+        sortedDataTest,
+        .{ &tree_data, &expected },
+    );
+}
+
+test "empty tree has null root" {
     var tree = Tree.init(testing.allocator);
     defer tree.deinit();
-    try tree.insert(2);
-    try tree.insert(1);
-    try tree.insert(3);
-    try tree.insert(6);
-    try tree.insert(7);
-    try tree.insert(5);
-    const expected = [_]i32{ 1, 2, 3, 5, 6, 7 };
-    const actual = try tree.sortedData();
-    defer testing.allocator.free(actual);
-    try testing.expectEqualSlices(i32, &expected, actual);
+    try testing.expectEqual(null, tree.root);
+}
+
+test "can sort data-can sort empty tree" {
+    const tree_data = [_]i32{};
+    const expected = [_]i32{};
+    try std.testing.checkAllAllocationFailures(
+        std.testing.allocator,
+        sortedDataTest,
+        .{ &tree_data, &expected },
+    );
 }
