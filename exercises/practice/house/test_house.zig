f75595d0862c932a855d38d0f53e9eeefce4ff50
Eric Willigers <keiravillekode@gmail.com>
2026-07-13 21:08:21 +1000

    check buffer usage
    We size the buffer to match the largest output.
    
    - bottle-song
    - food-chain
    - house
    - twelve-days

--- exercises/practice/house/test_house.zig
+++ exercises/practice/house/test_house.zig
@@ -3,144 +3,97 @@ const testing = std.testing;
 
 const house = @import("house.zig");
 
-test "verse one - the house that jack built" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
-        \\This is the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 1, 1);
+fn testRecite(expected: []const u8, start_verse: u32, end_verse: u32) !void {
+    var buffer: [2335]u8 = undefined;
+    const actual = try house.recite(&buffer, start_verse, end_verse);
+    try testing.expectEqual(@as([*]const u8, &buffer), actual.ptr);
     try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse one - the house that jack built" {
+    try testRecite(
+        \\This is the house that Jack built.
+    , 1, 1);
 }
 
 test "verse two - the malt that lay" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 2, 2);
-    try testing.expectEqualStrings(expected, actual);
+    , 2, 2);
 }
 
 test "verse three - the rat that ate" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 3, 3);
-    try testing.expectEqualStrings(expected, actual);
+    , 3, 3);
 }
 
 test "verse four - the cat that killed" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 4, 4);
-    try testing.expectEqualStrings(expected, actual);
+    , 4, 4);
 }
 
 test "verse five - the dog that worried" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 5, 5);
-    try testing.expectEqualStrings(expected, actual);
+    , 5, 5);
 }
 
 test "verse six - the cow with the crumpled horn" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 6, 6);
-    try testing.expectEqualStrings(expected, actual);
+    , 6, 6);
 }
 
 test "verse seven - the maiden all forlorn" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 7, 7);
-    try testing.expectEqualStrings(expected, actual);
+    , 7, 7);
 }
 
 test "verse eight - the man all tattered and torn" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 8, 8);
-    try testing.expectEqualStrings(expected, actual);
+    , 8, 8);
 }
 
 test "verse nine - the priest all shaven and shorn" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 9, 9);
-    try testing.expectEqualStrings(expected, actual);
+    , 9, 9);
 }
 
 test "verse 10 - the rooster that crowed in the morn" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 10, 10);
-    try testing.expectEqualStrings(expected, actual);
+    , 10, 10);
 }
 
 test "verse 11 - the farmer sowing his corn" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 11, 11);
-    try testing.expectEqualStrings(expected, actual);
+    , 11, 11);
 }
 
 test "verse 12 - the horse and the hound and the horn" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 12, 12);
-    try testing.expectEqualStrings(expected, actual);
+    , 12, 12);
 }
 
 test "multiple verses" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
         \\This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
         \\This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
         \\This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
         \\This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 4, 8);
-    try testing.expectEqualStrings(expected, actual);
+    , 4, 8);
 }
 
 test "full rhyme" {
-    const buffer_size = 4000;
-    var buffer: [buffer_size]u8 = undefined;
-    const expected: []const u8 =
+    try testRecite(
         \\This is the house that Jack built.
         \\This is the malt that lay in the house that Jack built.
         \\This is the rat that ate the malt that lay in the house that Jack built.
@@ -153,7 +106,5 @@ test "full rhyme" {
         \\This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
         \\This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
         \\This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    ;
-    const actual = try house.recite(&buffer, 1, 12);
-    try testing.expectEqualStrings(expected, actual);
+    , 1, 12);
 }
