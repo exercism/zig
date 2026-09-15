768d76fe8250320287b3e3d4bcaaaf52475edf5e
keiravillekode <keiravillekode@gmail.com>
2026-09-16 05:57:43 +1000

    etl: tests use defer (#583)
    Previously, tests could leak memory on failure
    https://forum.exercism.org/t/etl-exercise-on-zig-track-leaks-memory-in-tests/82336

--- generators/exercises/etl.py
+++ generators/exercises/etl.py
@@ -9,14 +9,14 @@ def gen_case(case):
 
     lines = [
         "    var legacy = std.AutoHashMap(i5, []const u8).init(testing.allocator);\n"
+        "    defer legacy.deinit();\n"
     ]
     for score in sorted(legacy):
         letters = legacy[score]
         lines.append(f"    try legacy.put({int(score)}, {zstr(''.join(letters))});\n")
-    lines.append("    var actual = try transform(testing.allocator, legacy);\n")
-    lines.append("    legacy.deinit();\n\n")
+    lines.append("\n    var actual = try transform(testing.allocator, legacy);\n")
+    lines.append("    defer actual.deinit();\n")
     lines.append(f"    try testing.expectEqual({len(expected)}, actual.count());\n")
     for letter, score in expected.items():
         lines.append(f"    try testing.expectEqual({score}, actual.get('{letter}'));\n")
-    lines.append("    actual.deinit();\n")
     return "".join(lines)
